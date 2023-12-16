ff82ca424f081a2c5ad5f77446661c8a4eb62b03
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-15 22:51:32 -0600

    hex_map_generator: Added logic to BattleMapPractice script to generate intended tile layout for even-rowed maps.

--- BattleMapPractice.gd
+++ BattleMapPractice.gd
@@ -5,12 +5,12 @@ const MAP_TILE := preload("MapTile.tscn")
 # The ratio between 
 # the distance from the center of a hexagon to one of its vertices and 
 # the distance from the center of a hexagon to the midpoint of one of its edges
-const HEX_EDGE_RATIO: float = 0.87
+const HEX_EDGE_RATIO: float = sqrt(3.0) / 2.0
 
 # The number of tiles along the X axis
-export(int, 1, 30) var x_count = 1
+export(int, 1, 30) var x_count = 2
 # The number of tiles along the Z axis
-export(int, 3, 30) var z_count = 3
+export(int, 1, 30) var z_count = 3
 
 
 # Instantiates the hex grid map tile at the specified offset
@@ -23,48 +23,57 @@ func _instantiate_tile(offset: Vector3):
 # Generates a hex grid with an odd number of tiles on the z-axis
 func _generate_odd_grid():
 	var map_tile_offset: Vector3
-	var x: int
-	# Keeps track of if an extra tile needs to be added on an even z index
-	var extra_tile_added: bool
+	# Keeps track of if an extra tile needs to be prepended on an odd z index
+	var extra_tile_prepended: bool
 	
 	for z in z_count:
-		x = 0
-		extra_tile_added = false
 		map_tile_offset = Vector3.ZERO
 		map_tile_offset.z = 1.5 * z
-		while x < x_count:
+		extra_tile_prepended = false
+		for x in x_count:
 			map_tile_offset.x = 2 * HEX_EDGE_RATIO * x
-			# Adjust the x offset based on the current z count
+			# Add an extra tile before the start of odd rows
 			if z % 2 != 0: 
-				if extra_tile_added:
+				if extra_tile_prepended:
 					map_tile_offset.x += HEX_EDGE_RATIO
 				else:
-					# Add an extra tile before the start of the row
 					map_tile_offset.x -= HEX_EDGE_RATIO
-					extra_tile_added = true
-					x-=1
-			x+=1
+					extra_tile_prepended = true
+					_instantiate_tile(map_tile_offset)
+					map_tile_offset.x += 2 * HEX_EDGE_RATIO
 			_instantiate_tile(map_tile_offset)
 
 
 # Generates a hex grid with an even number of tiles on the z-axis
 func _generate_even_grid():
-	pass
+	var map_tile_offset: Vector3
+	# Keeps track of if an extra tile needs to be prepended on an odd z index
+	var extra_tile_prepended: bool
+	
+	for z in z_count:
+		map_tile_offset = Vector3.ZERO
+		map_tile_offset.z = 1.5 * z
+		extra_tile_prepended = false
+		for x in x_count:
+			map_tile_offset.x = 2 * HEX_EDGE_RATIO * x
+			# Prepends an extra tile on odd rows except for the last row
+			if z % 2 != 0:
+				if z == (z_count - 1) or extra_tile_prepended:
+					map_tile_offset.x += HEX_EDGE_RATIO
+				else:
+					map_tile_offset.x -= HEX_EDGE_RATIO
+					extra_tile_prepended = true
+					_instantiate_tile(map_tile_offset)
+					map_tile_offset.x += 2 * HEX_EDGE_RATIO
+			# Appends an extra tile on even rows except for the first row
+			elif z != 0 and x == (x_count - 1):
+				_instantiate_tile(map_tile_offset)
+				map_tile_offset.x += 2 * HEX_EDGE_RATIO
+			_instantiate_tile(map_tile_offset)
+
 
 # Generates the hex grid map basd off of x_count and z_count
 func _generate_grid():
-	#var map_tile_offset: Vector3
-	#
-	# Generata a grid of hex tiles
-	#for z in z_count:
-	#	map_tile_offset = Vector3.ZERO
-	#	map_tile_offset.z = 1.5 * z
-	#	for x in x_count:
-	#		map_tile_offset.x = 2 * HEX_EDGE_RATIO * x
-	#		# Adjust the x offset based on the current z count
-	#		if z % 2 != 0: map_tile_offset.x += HEX_EDGE_RATIO
-	#		
-	#		_instantiate_tile(map_tile_offset)
 	if z_count % 2 != 0: _generate_odd_grid()
 	else: _generate_even_grid()
 
