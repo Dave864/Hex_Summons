a13e15a0d3e09d9c6dffc2de72c228a2e35dfe3c
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-15 21:27:45 -0600

    hex_map_generator: Added logic to BattleMapPractice script to generate intended tile layout for odd-rowed maps.

--- BattleMapPractice.gd
+++ BattleMapPractice.gd
@@ -13,23 +13,60 @@ export(int, 1, 30) var x_count = 1
 export(int, 3, 30) var z_count = 3
 
 
-# Generates the hex grid map basd off of x_count and z_count
-func _generate_grid():
+# Instantiates the hex grid map tile at the specified offset
+func _instantiate_tile(offset: Vector3):
+	var map_tile = MAP_TILE.instance()
+	$Tiles.add_child(map_tile)
+	map_tile.translate_object_local(offset)
+
+
+# Generates a hex grid with an odd number of tiles on the z-axis
+func _generate_odd_grid():
 	var map_tile_offset: Vector3
+	var x: int
+	# Keeps track of if an extra tile needs to be added on an even z index
+	var extra_tile_added: bool
 	
-	# Generata a grid of hex tiles
 	for z in z_count:
+		x = 0
+		extra_tile_added = false
 		map_tile_offset = Vector3.ZERO
 		map_tile_offset.z = 1.5 * z
-		for x in x_count:
+		while x < x_count:
 			map_tile_offset.x = 2 * HEX_EDGE_RATIO * x
 			# Adjust the x offset based on the current z count
-			if z % 2 != 0:
-				map_tile_offset.x += HEX_EDGE_RATIO
-			
-			var map_tile = MAP_TILE.instance()
-			$Tiles.add_child(map_tile)
-			map_tile.translate_object_local(map_tile_offset)
+			if z % 2 != 0: 
+				if extra_tile_added:
+					map_tile_offset.x += HEX_EDGE_RATIO
+				else:
+					# Add an extra tile before the start of the row
+					map_tile_offset.x -= HEX_EDGE_RATIO
+					extra_tile_added = true
+					x-=1
+			x+=1
+			_instantiate_tile(map_tile_offset)
+
+
+# Generates a hex grid with an even number of tiles on the z-axis
+func _generate_even_grid():
+	pass
+
+# Generates the hex grid map basd off of x_count and z_count
+func _generate_grid():
+	#var map_tile_offset: Vector3
+	#
+	# Generata a grid of hex tiles
+	#for z in z_count:
+	#	map_tile_offset = Vector3.ZERO
+	#	map_tile_offset.z = 1.5 * z
+	#	for x in x_count:
+	#		map_tile_offset.x = 2 * HEX_EDGE_RATIO * x
+	#		# Adjust the x offset based on the current z count
+	#		if z % 2 != 0: map_tile_offset.x += HEX_EDGE_RATIO
+	#		
+	#		_instantiate_tile(map_tile_offset)
+	if z_count % 2 != 0: _generate_odd_grid()
+	else: _generate_even_grid()
 
 
 # Called when the node enters the scene tree for the first time.
