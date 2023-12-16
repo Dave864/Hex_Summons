26aceecbe672821d529c41dc12f34ecb83a4aa83
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-16 00:27:43 -0600

    map_tile: Added animation and signal that indicates when a tile is selected.

--- TileSelector.gd
+++ TileSelector.gd
@@ -1,12 +1,18 @@
 extends Area
 
+# Indicates that the tile was selected
+signal tile_selected
 
 # The material to use when the mouse hovers over the tile
 export var hover_material: SpatialMaterial
 
+# Flag to indicate that the mouse cursor is over the tile
+var mouse_hover: bool
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	mouse_hover = false;
 	$MeshInstance.set_surface_material(0, hover_material)
 	$MeshInstance.hide()
 
@@ -16,10 +22,17 @@ func _ready():
 #	pass
 
 
-func _on_TileSelector_mouse_entered():
+func _input(event):
+	if event is InputEventMouseButton and mouse_hover:
+		$AnimationPlayer.play("selected")
+		emit_signal("tile_selected")
 
+
+func _on_TileSelector_mouse_entered():
+	mouse_hover = true
 	$MeshInstance.show()
 
 
 func _on_TileSelector_mouse_exited():
+	mouse_hover = false
 	$MeshInstance.hide()
