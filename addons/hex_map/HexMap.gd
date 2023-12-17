391007728ed05cd7675d51993a035fef4e748876
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-17 07:11:20 -0600

    map_tile: Refactored the HexMap script to generate tiles in a traditional grid layout.

--- addons/hex_map/HexMap.gd
+++ addons/hex_map/HexMap.gd
@@ -42,14 +42,7 @@ func _initial_grid_generation():
 func _calculate_grid_start() -> Vector3:
 	var origin_offset = Vector3(2 * HEX_EDGE_RATIO, 0.0, 1.0)
 	origin_offset.z -= ((3.0 * z_count) + 1.0) / 4.0
-	if _is_even_grid():
-		if z_count == 2:
-			origin_offset.x -= (x_count + 1.5) * HEX_EDGE_RATIO
-		else:
-			origin_offset.x -= (x_count + (3.5/2.0)) * HEX_EDGE_RATIO
-		origin_offset.x -= 0.0
-	else:
-		origin_offset.x -= (x_count + 1.0) * HEX_EDGE_RATIO
+	origin_offset.x -= (x_count if z_count == 2 else (x_count + 1.5)) * HEX_EDGE_RATIO
 	return origin_offset
 
 
@@ -62,45 +55,19 @@ func _update_grid_start():
 func _generate_grid():
 	var map_tile_offset: Vector3
 	var index: int = 0
-	# Keeps track of if an extra tile needs to be prepended on an odd z index
+	# Keeps track of if an extra tile needs to be prepended
 	var extra_tile_prepended: bool
 	
+	# Calculates the position for each tile relative to origin
+	# and adds extra tiles as needed
 	for z in z_count:
 		map_tile_offset = Vector3.ZERO
 		map_tile_offset.z = 1.5 * z
 		extra_tile_prepended = false
 		for x in x_count:
 			map_tile_offset.x = 2 * HEX_EDGE_RATIO * x
-			# Generate a hex row for a grid with an even number of rows
-			if _is_even_grid():
-				# Prepends an extra tile on odd rows except for the last row
-				if !_is_even(z):
-					if z == (z_count - 1) or extra_tile_prepended:
-						map_tile_offset.x += HEX_EDGE_RATIO
-					else:
-						map_tile_offset.x -= HEX_EDGE_RATIO
-						extra_tile_prepended = true
-						_instantiate_tile(map_tile_offset, index)
-						map_tile_offset.x += 2 * HEX_EDGE_RATIO
-						index += 1
-				# Appends an extra tile on even rows except for the first row
-				elif z != 0 and x == (x_count - 1):
-					_instantiate_tile(map_tile_offset, index)
-					map_tile_offset.x += 2 * HEX_EDGE_RATIO
-					index += 1
-			# Generate a hex row for a grid with an odd number of rows
-			else:
-				map_tile_offset.x = 2 * HEX_EDGE_RATIO * x
-				# Add an extra tile before the start of odd rows
-				if !_is_even(z): 
-					if extra_tile_prepended:
-						map_tile_offset.x += HEX_EDGE_RATIO
-					else:
-						map_tile_offset.x -= HEX_EDGE_RATIO
-						extra_tile_prepended = true
-						_instantiate_tile(map_tile_offset, index)
-						map_tile_offset.x += 2 * HEX_EDGE_RATIO
-						index += 1
+			if !_is_even(z):
+				map_tile_offset.x += HEX_EDGE_RATIO
 			_instantiate_tile(map_tile_offset, index)
 			index += 1
 
@@ -115,7 +82,8 @@ func _is_even(number) -> bool:
 	return number % 2 == 0
 
 
-# Instantiates the hex grid map tile at the specified offset
+# Instantiates the hex grid map tile at the specified offset with the HexMap
+# node position being considered origin
 func _instantiate_tile(offset: Vector3, index: int):
 	var map_tile = MAP_TILE.instance()
 	add_child(map_tile)
@@ -123,9 +91,7 @@ func _instantiate_tile(offset: Vector3, index: int):
 	map_tile.set_index(index)
 	
 	# Position the tile to the appropriate position
-	map_tile.translate_object_local(offset)
-	map_tile.translate_object_local(_grid_start)
-
+	map_tile.translate_object_local(offset + _grid_start)
 
 
 # Goes through the Map Tiles and establishes what each one's adjacent tiles are
