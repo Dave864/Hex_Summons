b4d50d796228a406d3db9cdf5b09da309cf697d3
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-19 16:16:53 -0600

    hex_map: Created Creature scene.

---
+++ Creature.gd
@@ -0,0 +1,19 @@
+extends Area
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export(int) var start_tile_index = 0
+
+var _current_tile_index: int = start_tile_index
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
