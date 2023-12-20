cd163a2a7a90370e6fc9120e0ee5278a079ead58
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-19 22:13:32 -0600

    hex_map: Added parameter to MapTile to keep track of tile's cubic coordinate.

--- addons/hex_map/map_tile/MapTile.gd
+++ addons/hex_map/map_tile/MapTile.gd
@@ -1,5 +1,4 @@
 tool
-class_name MapTile
 extends Spatial
 
 
@@ -11,19 +10,34 @@ signal tile_selected(tile_index)
 #  5 |  | 2
 #  4  \/  3
 var _adjacent_tiles: Array = [null, null, null, null, null, null]
-# Tracks the index position of the map tile when it is part 
+# The index position of the map tile when it is part 
 # of a collection of tiles
 var _index: int setget set_index, get_index
+# The cube coordinates of the map tile
+#     -z
+# +y   |  +x
+#   \ / \ /
+#    |   |
+#   / \ / \
+# -x   |  -y
+#     +z
+var _cube_coord: Vector3 setget set_cube_coord, get_cube_coord
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	set_index(-1)
+	set_cube_coord(Vector3.ZERO)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+# Signals which tile was selected
+func _on_TileSelector_selected():
+	emit_signal("tile_selected", _index)
 
 
 # Gets the adjacent tile of the specified position
@@ -36,21 +50,26 @@ func set_adjacent_tile(position: int, map_tile: Spatial):
 	_adjacent_tiles[position] = map_tile
 
 
-# Gets the index value of the Map Tile
-func get_index():
+# Get the index value of the MapTile
+func get_index() -> int:
 	return _index
 
 
-# Sets the index value of the Map Tile
+# Set the index value of the MapTile
 func set_index(value: int):
 	_index = value
+
+
+# Get the cube coordinates of the MapTile
+func get_cube_coord() -> Vector3:
+	return _cube_coord
+
+
+# Set the cube coordinates of the MapTile
+func set_cube_coord(value: Vector3):
+	_cube_coord = value
 
 
 # Checks whether the Map Tile is an active element of the map
 func is_active() -> bool:
 	return visible
-
-
-# Signals which tile was selected
-func _on_TileSelector_selected():
-	emit_signal("tile_selected", _index)
