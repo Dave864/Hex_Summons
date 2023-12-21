f4f7a838f550a9fb3e0eb8fb0f950bcc83f21e7c
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-21 12:11:47 -0600

    hex_map_refactor: Implemented logic to enable Selector node mesh to snap to a tile when said tile has been passed over.

--- hex_map_base/selector_node/selector.gd
+++ hex_map_base/selector_node/selector.gd
@@ -9,6 +9,8 @@ passed over.
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var _snap_selector: bool = false
+var _snap_position: Vector3 = Vector3.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,9 +19,13 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	translation = $MousePosition.get_mouse_position()
+	if _snap_selector:
+		var new_position: Vector3 = _snap_position - translation
+		$SelectorShape.translation = Vector3(new_position.x, 0.125, new_position.z)
 
 
 func _on_Selector_area_entered(area):
-	pass # Replace with function body.
+	_snap_selector = true
+	_snap_position = area.translation
