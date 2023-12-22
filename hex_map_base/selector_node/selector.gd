17123f54a6b45ca4e8e4d841a734fa3aab3d8004
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-22 04:40:09 -0600

    selector: Added logic to Selector scene that implements basic selection functionality.

--- hex_map_base/selector_node/selector.gd
+++ hex_map_base/selector_node/selector.gd
@@ -1,31 +1,39 @@
 class_name Selector
 extends Area
 """
-Moves around the map based on mouse movement and detects when a tile has been
+Moves around the map based on mouse movement and detects when a MapTile has been
 passed over.
 """
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var _snap_selector: bool = false
+signal tile_selected(map_tile)
+
+# Indicates when the SelectorShape should lock its position to that of the
+# passed over tile.
+var _snap_to_position: bool = false
+# The position the SelectorShape locks to.
 var _snap_position: Vector3 = Vector3.ZERO
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+# The MapTile that was last passed over
+var _tile: MapTile = null
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	translation = $MousePosition.get_mouse_position()
-	if _snap_selector:
+	if _snap_to_position:
 		var new_position: Vector3 = _snap_position - translation
 		$SelectorShape.translation = Vector3(new_position.x, 0.125, new_position.z)
 
 
+func _input(event):
+	if event is InputEventMouseButton:
+		# On mouse left click
+		if event.button_index == BUTTON_LEFT and event.is_pressed():
+			emit_signal("tile_selected", _tile)
+			$AnimationPlayer.play("selected")
+
+
 func _on_Selector_area_entered(area):
-	_snap_selector = true
+	_snap_to_position = true
 	_snap_position = area.translation
+	_tile = area
