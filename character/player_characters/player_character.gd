2462dae11e7030d55420943aa5655b45cffee398
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-27 17:31:49 -0600

    character: Created a player character scene.

---
+++ character/player_characters/player_character.gd
@@ -0,0 +1,12 @@
+class_name PlayerCharacter
+extends Character
+
+
+var level: int = 1
+# The current player class; determines stat adjusters and abilities.
+#var player_class
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
