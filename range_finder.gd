d9d488128e5d703136788e210d4464935d3c13e7
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-29 08:48:35 -0600

    encounter-hex_map: Updated range_finder class to account for inactive and occupied tiles.

--- range_finder.gd
+++ range_finder.gd
@@ -18,6 +18,55 @@ func _init(x_value: int, z_value: int, initial_map: Array = []):
 	set_map_tiles(initial_map)
 
 
+func set_z_count(value: int):
+	_z_count = value
+
+
+func set_x_count(value: int):
+	_x_count = value
+
+
+func set_map_tiles(new_map: Array):
+	_map_tiles = new_map
+	refresh_astar_connections()
+
+
+# Recalculates the astar connnections for the map in its current state.
+func refresh_astar_connections():
+	# Empty out the current astar map and resize if necessary.
+	clear()
+	if get_point_capacity() < _map_tiles.size():
+		reserve_space(_map_tiles.size())
+	
+	# Add the tiles to the astar map.
+	for tile in _map_tiles:
+		# Do not add tile if it is occupied or inactive.
+		if tile.is_active() and tile.get_occupant() != null:
+			add_point(tile.get_index(), tile.translation)
+	
+	# Set up the connections for the astar map.
+	for tile in _map_tiles:
+		# Skip over inactive or occupied tiles.
+		if tile.is_active() and tile.get_occupant() == null:
+			for neighbor in tile.get_adjacent():
+				# Do not connect inactive or occupied tiles.
+				if (
+					neighbor != null or 
+					!tile.is_active() or 
+					tile.get_occupant() != null
+				):
+					connect_points(
+						tile.get_index(), 
+						neighbor.get_index()
+					)
+
+
+# Calculate the points along the path from the tile at the start index to the
+# tile at the end index. The points are returned as an array of Vector3's.
+func calculate_path(start_index: int, end_index: int) -> PoolVector3Array:
+	return get_point_path(start_index, end_index)
+
+
 func _compute_cost(u, v):
 	return _cube_dist(u, v)
 
@@ -51,37 +100,3 @@ func _cube_to_index(coord: Vector3) -> int:
 	var z_pos: int = int(coord.y + (coord.x - (int(coord.x) & 1)) / 2.0)
 	var x_pos: int = int(coord.x)
 	return (z_pos * _x_count) + x_pos
-
-
-func set_z_count(value: int):
-	_z_count = value
-
-
-func set_x_count(value: int):
-	_x_count = value
-
-
-func set_map_tiles(new_map: Array):
-	_map_tiles = new_map
-	clear()
-	if get_point_capacity() < _map_tiles.size():
-		reserve_space(_map_tiles.size())
-	
-	# Add the tiles to the astar map
-	for tile in _map_tiles:
-		add_point(tile.get_index(), tile.translation)
-	
-	# Set up the connections for the astar map
-	for tile in _map_tiles:
-		for neighbor in tile.get_adjacent():
-			if neighbor != null:
-				connect_points(
-					tile.get_index(), 
-					neighbor.get_index()
-				)
-
-
-# Calculate the points along the path from the tile at the start index to the
-# tile at the end index. The points are returned as an array of Vector3's.
-func calculate_path(start_index: int, end_index: int) -> PoolVector3Array:
-	return get_point_path(start_index, end_index)
