2d8ff907a7f0c3286c073e7a2eb677864ee87aba
Anthony David Macpherson <dmacpherson864@gmail.com>
2023-12-30 00:22:08 -0600

    encounter-states: Created new state machine for hanling encounter scenes.

--- autoloads/state_machine_bus.gd
+++ autoloads/state_machine_bus.gd
@@ -3,6 +3,7 @@ extends Node
 
 # Keeps track of the current states of various FSMs within an encounter scene.
 var encounter_states: Dictionary = {
+	"Encounter": null,
 	"Selector": null,
 	"PlayerCharacter": null,
 	"EnemyCharacter": null,
---
+++ states/encounter/encounter_state.gd
@@ -0,0 +1,26 @@
+class_name EncounterState
+extends State
+"""
+Boilerplate class to get full autocompletion and type checks for an 
+`encounter` when coding an encounter's states.
+"""
+
+
+# Typed reference to the Encounter node.
+var enc: Encounter
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# The states are children of a `Encounter` node so their `_ready()` 
+	# callback will execute first. That's why we wait for the `owner` 
+	# to be ready first.
+	yield(owner, "ready")
+	# The `as` keyword casts the `owner` variable to the `Encounter` type.
+	# If the `owner` is not an `Encounter`, we'll get `null`.
+	enc = owner as Encounter
+	# This check will tell us if we inadvertently assign a derived state script
+	# in a scene other than the PlayerCharacter scene, which would be 
+	# unintended. This can help prevent some bugs that are difficult to 
+	# understand.
+	assert(enc != null)
