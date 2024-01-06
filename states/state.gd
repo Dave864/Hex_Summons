775404797e8b691491eb76162d0f2149e6db5162
Anthony David Macpherson <dmacpherson864@gmail.com>
2024-01-06 06:51:08 -0600

    character-technique: Refactored comments and function positions.

--- states/state.gd
+++ states/state.gd
@@ -10,8 +10,9 @@ Virtual base class for all states.
 var state_machine = null
 
 
-# Virtual function. Called by the state machine upon changing the active state. The `msg` parameter
-# is a dictionary with arbitrary data the state can use to initialize itself.
+# Virtual function. Called by the state machine upon changing the active state. 
+# The `msg` parameter is a dictionary with arbitrary data the state can use to 
+# initialize itself.
 func enter(_msg := {}) -> void:
 	pass
 
@@ -31,7 +32,7 @@ func physics_update(_delta: float) -> void:
 	pass
 
 
-# Virtual function. Called by the state machine before changing the active state. Use this function
-# to clean up the state.
+# Virtual function. Called by the state machine before changing the active 
+# state. Use this function to clean up the state.
 func exit() -> void:
 	pass
