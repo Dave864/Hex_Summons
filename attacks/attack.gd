22567bc35ef2851035e14674fb88abcb09317414
Anthony David Macpherson <dmacpherson864@gmail.com>
2024-01-08 04:51:17 -0600

    character-technique: Updated attack stat ranges.

--- attacks/attack.gd
+++ attacks/attack.gd
@@ -10,11 +10,11 @@ range, and spread of an attack.
 # The bonus value of the attack. Added to the appropriate character stat to 
 # determine the attack's strength.
 export(int, 1, 1000) var atk_bonus = 1
-# How far away the attack can be placed. A value of zero indicates that the
-# attack origninates from the character's position.
-export(int, 20) var start_range = 0
+# How far away the attack can be placed. A value of one indicates that the
+# attack is adjacent to the character.
+export(int, 1, 20) var start_range = 1
 # The number of tiles emaniting outwards the attack affects.
-export(int, 1, 20) var reach = 1
+export(int, 20) var reach = 0
 # The number of tiles adjacent to the start position the attack effects.
 export(int, 1, 6) var spread = 1
 # The highlighter color used to indicate the attack's area of effect.
