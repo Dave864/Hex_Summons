6e1d9ca8ba1fb8f03c3758513c2294bb45662477
Dave864 <dmacpherson864@gmail.com>
2025-02-12 19:02:08 -0800

    user_interface-encounter: Added more documentation to the autoload buses.

--- autoloads/signal_bus.gd
+++ autoloads/signal_bus.gd
@@ -1,14 +1,12 @@
 extends Node
+"""
+A collection of signals that need to be referenced by multiple disjointed nodes.
+"""
 
 
-# Indicates that a map tile has been selected by the cursor
+# Signals most relevant in an Encouinter scene.
 signal tile_selected(info)
-
-# Indicates that a player character's turn has started
+signal selector_required()
 signal player_turn_started(player)
-
-# Indicates that a player character's turn has ended
 signal player_turn_ended(player)
-
-# Indicates that an enemy character's turn has started
 signal enemy_turn_started(path)
--- autoloads/state_machine_bus.gd
+++ autoloads/state_machine_bus.gd
@@ -1,5 +1,8 @@
 extends Node
-
+"""
+A collection of dictionaries that keep track of the current state of the FSMs
+within different scenes.
+"""
 
 # Keeps track of the current states of various FSMs within an encounter scene.
 var encounter_states: Dictionary = {
