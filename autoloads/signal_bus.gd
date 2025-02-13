391d712c33f7c403666aa406f2a099be427c037f
Dave864 <dmacpherson864@gmail.com>
2025-02-12 18:19:45 -0800

    user_interface-encounter: Created new 'Pause' state for selector to allow the selector to be swapped back to when needed during a player character turn.

--- autoloads/signal_bus.gd
+++ autoloads/signal_bus.gd
@@ -4,8 +4,11 @@ extends Node
 # Indicates that a map tile has been selected by the cursor
 signal tile_selected(info)
 
-# Indicates that the player character's turn has started
+# Indicates that a player character's turn has started
 signal player_turn_started(player)
 
-# Indicates that the enemy character's turn has started
+# Indicates that a player character's turn has ended
+signal player_turn_ended(player)
+
+# Indicates that an enemy character's turn has started
 signal enemy_turn_started(path)
