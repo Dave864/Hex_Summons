c64f639c4831cfe62a465b08189b488a6d61bc9a
Dave864 <dmacpherson864@gmail.com>
2025-02-23 19:18:44 -0800

    prototype: Created test players and enemies and added them to the practice encounter scene. Created a suite of actions for testing.

--- states/user_interface/encounter/Standby.gd
+++ states/user_interface/encounter/Standby.gd
@@ -12,7 +12,7 @@ to the `Wait` state when the user chooses to end their turn. Moves to the
 # The `msg` parameter is a dictionary with arbitrary data the state can use to 
 # initialize itself.
 func enter(_msg := {}) -> void:
-	_set_state_machine_bus(PAUSE)
+	_set_state_machine_bus(STANDBY)
 	encounter_ui.sub_options.show()
 	encounter_ui.options.show()
 	# This signal is used by other states and will be disconnected to avoid
