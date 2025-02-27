7307c0f1f80e668ab38881a344ec4c55265733f1
Dave864 <dmacpherson864@gmail.com>
2025-02-26 18:03:17 -0800

    prototype: Updated RingRange scene to use CollisionPolygon to determine the effect collision shape.

--- actions/range_type/ring_range.gd
+++ actions/range_type/ring_range.gd
@@ -20,7 +20,18 @@ func set_distance(val: int) -> void:
 func _update_collision_shape() -> void:
 	if distance == 0:
 		_cs_as_point()
-		$CollisionShape.translation = Vector3.ZERO
+		$CollisionPolygon.translation = Vector3.ZERO
 	else:
-		var c_shape: ConvexPolygonShape = ConvexPolygonShape.new()
-		$CollisionShape.set_shape(c_shape)
+		var cs_points: PoolVector2Array = _cs_as_ring()
+		$CollisionPolygon.set_depth(_cs_height)
+		$CollisionPolygon.set_polygon(cs_points)
+		$CollisionPolygon.rotation_degrees = Vector3(90.0, 0.0 ,0.0)
+
+
+# Draw the collision shape as a hexagonal ring.
+func _cs_as_ring() -> PoolVector2Array:
+	var cs_shape: PoolVector2Array = []
+	cs_shape.resize(6)
+	for i in range(6):
+		cs_shape[i] = Vector2(_tile_distance * distance, 0.0).rotated(deg2rad(60 * i))
+	return cs_shape
