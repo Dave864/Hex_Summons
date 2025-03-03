d32928aaf682421e4dfcf037ae07c979a30d5754
Dave864 <dmacpherson864@gmail.com>
2025-03-02 20:10:20 -0800

    composition_refactor: Refactored action script to utilize new range_type nodes. Updated each action to use the new system.

--- actions/action.gd
+++ actions/action.gd
@@ -1,38 +1,70 @@
 class_name Action
 extends Node
 """
-Describes the range, damage profile, and effects of an action.
+Describes the range and effects of an action.
 """
 
 
+"""
+TODO: Implement logic to use stats nodes to define action effect
+"""
 export(int, 1, 1000) var power
+# Flag that denotes if the emission is fixed to the center of the area
+export(bool) var emit_from_center
 
-var targeted_tiles: Array = []
+# The tiles that are possible for a effect emission.
+var area_tiles: Array = []
+# The area that is ignored when determining the possible tiles for effect emmision.
+var _dead_range: AreaRange
+# The area specifying the possible tiles for effect emmision.
+var _area_range: AreaRange
 
-onready var range_data: AreaRange = $EmissionPoint/Range
+# The area specifying the tiles affected by the effect.
+onready var effect_range: AreaRange = $EmissionPoint/EffectRange
+# The point the effect is emitted from.
 onready var emission_pt: EmissionPoint = $EmissionPoint
 
 
-# Clear out the array of target tiles.
-func clear_targets() -> void:
-	targeted_tiles.clear()
+func _ready() -> void:
+	# No DeadRange node indicates no dead range.
+	_dead_range = get_node_or_null("DeadRange")
+	_area_range = get_node("AreaRange")
 
 
-func enable_collision() -> void:
+func _process(_delta) -> void:
+	area_tiles.clear()
+	area_tiles = _area_range.tile_ids.duplicate(true)
+	if _dead_range:
+		for i in _dead_range.tile_ids:
+			area_tiles.erase(i)
+
+
+func enable_area_collision() -> void:
+	_area_range.set_monitoring(true)
+	if _dead_range:
+		_dead_range.set_monitoring(true)
+
+
+func disable_area_collision() -> void:
+	_area_range.set_monitoring(false)
+	if _dead_range:
+		_dead_range.set_monitoring(false)
+
+
+func enable_effect_collision() -> void:
 	emission_pt.get_node("Area").set_monitoring(true)
-	range_data.set_monitoring(true)
+	effect_range.set_monitoring(true)
 
 
-func disable_collision() -> void:
+func disable_effect_collision() -> void:
 	emission_pt.get_node("Area").set_monitoring(false)
-	range_data.set_monitoring(false)
-	clear_targets()
+	effect_range.set_monitoring(false)
 
 
-# Rotates the range area along the y-axis to align it with specified point.
+# Rotates the emission along the y-axis to align it with specified point.
 # Will only affect cardinal_range areas.
 func align_to_point(point: Vector3) -> void:
-	if range_data is CardinalRange:
+	if _area_range is CardinalRange:
 		point.y = 0.0
 		var emission_pos: Vector3 = emission_pt.translation
 		emission_pos.y = 0.0
@@ -42,17 +74,3 @@ func align_to_point(point: Vector3) -> void:
 			Vector3.RIGHT.angle_to(direction)
 		)
 		emission_pt.rotation_degrees = rotation
-
-
-func _ready() -> void:
-	$EmissionPoint/Range.connect("area_entered", self, "_on_Range_area_entered")
-	$EmissionPoint/Range.connect("area_exited", self, "_on_Range_area_exited")
-	disable_collision()
-
-
-func _on_Range_area_entered(tile: Area) -> void:
-	targeted_tiles.append(tile.get_map_index())
-
-
-func _on_Range_area_exited(tile: Area) -> void:
-	targeted_tiles.erase(tile.get_map_index())
