d0c4f41191d99ab5ea92757f3161b74ad8638d59
Dave864 <dmacpherson864@gmail.com>
2025-03-07 22:19:26 -0800

    prototype_1.01: Began working on new UI element, SubOptionsButton. This element will be designed to enable the SubOptions EncounterUI element to be able to identify which sub-option is being interacted with.

---
+++ user_interface/encounter/SubOptionsButton/sub_options_button.gd
@@ -0,0 +1,20 @@
+extends MarginContainer
+class_name SubOptionsButton
+"""
+Button that describes a possible sub-option for a given option.
+"""
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
