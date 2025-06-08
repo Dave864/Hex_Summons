8f7c3334a156efa01952eddf578f7d1ad6380a13
Dave864 <dmacpherson864@gmail.com>
2025-06-07 17:29:03 -0700

    composition_refactor: Updated the rescource script stat to include references to all magic and resistance stats. Updated the stat_types resources to all be of type Stat and to represent their corresponding stat.

--- character/scripts/stat.gd
+++ character/scripts/stat.gd
@@ -6,6 +6,25 @@ Defines the stats used by all characters: Health, Attack, Defense, Agility, Move
 
 
 # The core stats used by characters and actions.
-enum Type {CUR_HEALTH, MAX_HEALTH, ATTACK, DEFENSE, AGILITY, MOVEMENT}
+enum Type {
+	CUR_HEALTH,
+	MAX_HEALTH,
+	ATTACK,
+	DEFENSE,
+	AGILITY,
+	MOVEMENT,
+	MAGIC_EARTH,
+	MAGIC_FIRE,
+	MAGIC_WATER,
+	MAGIC_WIND,
+	MAGIC_LIGHT,
+	MAGIC_DARK,
+	RES_EARTH,
+	RES_FIRE,
+	RES_WATER,
+	RES_WIND,
+	RES_LIGHT,
+	RES_DARK,
+}
 
 export(Type) var type = Type.CUR_HEALTH
