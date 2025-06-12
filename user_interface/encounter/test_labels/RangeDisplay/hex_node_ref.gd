8896313ec7680c177e9fb57a0b6e7e5ba50a549c
Dave864 <dmacpherson864@gmail.com>
2025-06-12 01:28:55 -0700

    composition_refactor: Created new object that will represent a map tile in a matrix. Intended to be used for determining range display details. Added all current actions to the RangeDisplay for testing purposes. Updated RangeDisplay to accept a path to an Action.

---
+++ user_interface/encounter/test_labels/RangeDisplay/hex_node_ref.gd
@@ -0,0 +1,111 @@
+class_name HexNodeRef
+extends Object
+"""
+Keeps track of the adjacenct indexes for a given index. Meant to be used in a
+matrix representing a grid of hex tiles.
+"""
+
+
+const EMPTY_REF: Vector2 = Vector2(-1, -1)
+
+# X coordinate represents the column, Y coordinate represents the row.
+var index_pos: Vector2 = Vector2.ZERO
+
+# References the hex nodes that are adjacent to this one.
+#  0  / \  1
+#  5 |   | 2
+#  4  \ /  3
+var _neighbors: Array = [
+		EMPTY_REF,
+		EMPTY_REF,
+		EMPTY_REF,
+		EMPTY_REF,
+		EMPTY_REF,
+		EMPTY_REF
+]
+
+
+func get_neighbors() -> Array:
+	return _neighbors
+
+
+func _init(new_index: Vector2, row_count: int, col_count: int) -> void:
+	index_pos = new_index
+	_set_neighbors(row_count, col_count)
+
+
+# Sets the neighbors based on the size of the matrix.
+func _set_neighbors(row_count: int, col_count: int) -> void:
+	_neighbors[2] = (
+			Vector2(index_pos.x + 1, index_pos.y) if index_pos.x < col_count - 1
+			else EMPTY_REF
+	)
+	_neighbors[5] = (
+			Vector2(index_pos.x - 1, index_pos.y) if index_pos.x > 0
+			else EMPTY_REF
+	)
+	# Index is on top row.
+	if index_pos.y == 0:
+		_set_top_row_neighbors()
+	# Index is on bottom row.
+	elif index_pos.y == row_count - 1:
+		_set_bottom_row_neighbors(col_count)
+	# Index is not in border row.
+	else:
+		_set_neighbors_middle(col_count)
+
+
+# Sets the neigbors when the index_pos is in the top row.
+func _set_top_row_neighbors() -> void:
+	_neighbors[0] = EMPTY_REF
+	_neighbors[1] = EMPTY_REF
+	_neighbors[3] = Vector2(index_pos.x, index_pos.y + 1)
+	_neighbors[4] = (
+			EMPTY_REF if index_pos.x == 0
+			else Vector2(index_pos.x - 1, index_pos.y + 1)
+	)
+
+
+# Sets the neigbors when the index_pos is on the bottom row.
+func _set_bottom_row_neighbors(col_count: int)-> void:
+	_neighbors[3] = EMPTY_REF
+	_neighbors[4] = EMPTY_REF
+	
+	if int(index_pos.y) % 2 == 0:
+		_neighbors[0] = (
+				EMPTY_REF if index_pos.x == 0
+				else Vector2(index_pos.x - 1, index_pos.y - 1)
+		)
+		_neighbors[1] = Vector2(index_pos.x, index_pos.y - 1)
+	else:
+		_neighbors[0] = Vector2(index_pos.x - 1, index_pos.y - 1)
+		_neighbors[1] = (
+				EMPTY_REF if index_pos.x == col_count -1
+				else Vector2(index_pos.x + 1, index_pos.y - 1)
+		)
+
+
+# Sets the neigbors when the index_pos is in the middle of the map.
+func _set_neighbors_middle(col_count: int) -> void:
+	if int(index_pos.y) % 2 == 0:
+		_neighbors[0] = (
+				EMPTY_REF if index_pos.x == 0
+				else Vector2(index_pos.x - 1, index_pos.y - 1)
+		)
+		_neighbors[1] = Vector2(index_pos.x, index_pos.y - 1)
+		_neighbors[3] = Vector2(index_pos.x, index_pos.y + 1)
+		_neighbors[4] = (
+				EMPTY_REF if index_pos.x == 0
+				else Vector2(index_pos.x - 1, index_pos.y + 1)
+		)
+	else:
+		_neighbors[0] = Vector2(index_pos.x, index_pos.y - 1)
+		_neighbors[1] = (
+				EMPTY_REF if index_pos.x == col_count - 1
+				else Vector2(index_pos.x + 1, index_pos.y - 1)
+		)
+		_neighbors[3] = (
+				EMPTY_REF if index_pos.x == col_count - 1
+				else Vector2(index_pos.x + 1, index_pos.y + 1)
+		)
+		_neighbors[4] = Vector2(index_pos.x, index_pos.y + 1)
--- user_interface/encounter/test_labels/RangeDisplay/range_display.gd
+++ user_interface/encounter/test_labels/RangeDisplay/range_display.gd
@@ -6,6 +6,10 @@ of actions on a specific UI element.
 """
 
 
+export(NodePath) var action_ref
+var _action: Action = null
+
+var hex_radius = 3.0
 # References to the range details of an action.
 var _source_range: AreaRange = null
 var _dead_range: AreaRange = null
@@ -22,34 +26,46 @@ func update_range_display(action: Action) -> void:
 	update()
 
 
+func _ready() -> void:
+	_action = get_node(action_ref)
+#	update_range_display(_action)
+
+
 func _draw() -> void:
 	# Determine the configuration of tiles for the source range minus dead range.
 	_draw_range()
 
 
+func _determine_source_hexes() -> void:
+	if _source_range is CardinalArea:
+		pass
+	else:
+		pass
+
+
+func _determine_effect_hexes() -> void:
+	pass
+
+
 # Draws the array of hexagons that display the range of the action.
 func _draw_range() -> void:
-	_set_min_size(10, 10)
+	var _row_count: int = 10
+	var _col_count: int = 10
+	_set_min_size(_row_count, _col_count)
 	var center: Vector2 = Vector2.ZERO
-	for row in range(1, 11):
-		center.y = (
-				(Constants.DISPLAY_HEX_RADIUS * 2 + 0.5) * row \
-				+ (rect_size.y / 2 - 12 * Constants.DISPLAY_HEX_RADIUS)
-		)
-		center.x = (
-				Constants.DISPLAY_HEX_RADIUS * 2 if row % 2 != 0
-				else Constants.DISPLAY_HEX_RADIUS * 3
-		)
+	for row in range(1, _row_count + 1):
+		center.y = hex_radius * 2 * row
+		center.x = hex_radius * 2 if row % 2 != 0 else hex_radius * 3
 		_draw_filled_hex(Color.gray, center)
-		for col in 9:
-			center.x += Constants.DISPLAY_HEX_RADIUS * 2 + 0.5
+		for col in _col_count - 1:
+			center.x += hex_radius * 2 + 0.5
 			_draw_filled_hex(Color.gray, center)
 
 
 # Draw a colored outline of a hexagon.
 func _draw_hex_outline(color: Color, center: Vector2) -> void:
 	var hex_vertices: PoolVector2Array = _get_points_for_hex(center)
-	draw_polyline(hex_vertices, color, Constants.DISPLAY_HEX_RADIUS / 2)
+	draw_polyline(hex_vertices, color, hex_radius / 2)
 
 
 # Draws a filled colored hexagon.
@@ -61,7 +77,7 @@ func _draw_filled_hex(color: Color, center: Vector2) -> void:
 # Gets the points for a hexagon centered at a given point.
 func _get_points_for_hex(center: Vector2) -> PoolVector2Array:
 	var hex_vertices: PoolVector2Array = []
-	var top_vertex: Vector2 = Vector2(0.0, Constants.DISPLAY_HEX_RADIUS)
+	var top_vertex: Vector2 = Vector2(0.0, hex_radius)
 	for i in 7:
 		hex_vertices.append(top_vertex.rotated(i * PI / 3) + center)
 	return hex_vertices
@@ -70,6 +86,6 @@ func _get_points_for_hex(center: Vector2) -> PoolVector2Array:
 # Sets the minimum size for the display panel so that the drawn elements are
 # always within its bounds. 
 func _set_min_size(row_count: int, col_count: int) -> void:
-	var x_size: float = (Constants.DISPLAY_HEX_RADIUS + 0.5) * 2 * (col_count + 0.5)
-	var y_size: float = (Constants.DISPLAY_HEX_RADIUS + 0.5) * 2 * row_count
+	var x_size: float = hex_radius * 2 * col_count
+	var y_size: float = hex_radius * 2 * row_count
 	set_custom_minimum_size(Vector2(x_size, y_size))
