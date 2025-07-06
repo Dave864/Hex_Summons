1ff72f1c4714eea62368699f3a9e757d9abde8e4
Dave864 <dmacpherson864@gmail.com>
2025-07-05 20:01:15 -0700

    p0.05-effects: Moved the stat resource script into the stat_types folder. Created strength_calculators folder in effects to store the various ways of determining effect strength.

--- effects/scripts/effect_aspect.gd
+++ effects/scripts/effect_aspect.gd
@@ -11,7 +11,7 @@ export(Resource) var stat_affected = null
 # How the targeted stat is modified.
 export(Constants.Operation) var operation = Constants.Operation.SET
 # The method that determines the strength of this effect.
-export(Resource) var strength_calculation = null
+export(Resource) var calculation_method = null
 # Flag that indicates if this effect is resisted by the target
 export(bool) var resisted = true
 # How many turns does this effect last after application. A value of zero means
@@ -27,7 +27,7 @@ var _action_potency: Potency = null setget set_action_potency
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_check_for_required_parameters()
-	strength_calculation.check_for_required_resources()
+	calculation_method.check_for_required_resources()
 
 
 # Updates the source character stats of this effect aspect.
@@ -42,7 +42,7 @@ func set_action_potency(new_potency: Potency) -> void:
 
 # Determines the numerical result of the effect on a target set of character stats.
 func effect_on_target(target_stats: CharacterStats) -> int:
-	return strength_calculation.process_operation(
+	return calculation_method.process_operation(
 			_source_stats,
 			target_stats,
 			stat_affected,
@@ -63,10 +63,10 @@ func _check_for_required_parameters() -> void:
 			"Error: Effect %s stat_affected is not a Stat." % [name]
 	)
 	assert(
-			strength_calculation != null,
+			calculation_method != null,
 			ErrorUtil.missing_required_parameter(name, "strength_calculation")
 	)
 	assert(
-			strength_calculation is StrengthCalculation,
+			calculation_method is StrengthCalculation,
 			"Error: Effect %s strength_calculation is not a StrengthCalculation resource." % [name]
 	)
