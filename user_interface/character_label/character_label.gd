f629185d300b2fd41e4d7cade857a86d042c6d24
Dave864 <dmacpherson864@gmail.com>
2025-07-15 23:49:19 -0700

    p0.05-initiative: Added CanvasLayer node to CharacterLabel so that the label can go behind other UI elements. Updated script to account for new node structure. Added a CharacterLabel to TestPlayer1 for testing.

--- user_interface/character_label/character_label.gd
+++ user_interface/character_label/character_label.gd
@@ -10,22 +10,37 @@ export(float, 0.0, 50.0) var y_offset = 0.0
 export(Character.Type) var character_type = Character.Type.NONE
 export(NodePath) var character_pos_ref = null
 
-var _character_pos: Position3D = null
+var _char_pos: Position3D = null
 
-onready var _initiative_label: Label = $HBoxContainer/InitiativeLabel
-onready var _health_bar: ProgressBar = $HBoxContainer/HealthBar
+onready var _data_container: HBoxContainer = $CanvasLayer/HBoxContainer
+onready var _initiative_label: Label = $CanvasLayer/HBoxContainer/InitiativeLabel
+onready var _health_bar: ProgressBar = $CanvasLayer/HBoxContainer/HealthBar
 onready var _camera: Camera = get_viewport().get_camera()
+
+
+# Sets the value of the current health.
+func set_cur_health(value: int) -> void:
+	_health_bar.set_value_no_signal(value)
+
+
+# Sets the value of max health.
+func set_max_health(value: int) -> void:
+	_health_bar.max_value = value
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_check_for_required_parameters()
-	_character_pos = get_node(character_pos_ref)
+	_char_pos = get_node(character_pos_ref)
+	_set_health_bar_color()
 
 
 # Moves this element so that it is always above the character position.
 func _physics_process(_delta: float) -> void:
-	rect_position = _camera.unproject_position(_character_pos.translation)
+	var _char_origin: Vector3 = _char_pos.global_transform.origin
+	_data_container.visible = not _camera.is_position_behind(_char_origin)
+	var r_pos: Vector2 = _camera.unproject_position(_char_pos.global_translation)
+	_data_container.rect_position = r_pos
 
 
 # Sets the modulation color of the health bar based on the character type.
@@ -48,12 +63,12 @@ func _check_for_required_parameters() -> void:
 
 # Updates the current health value of the label.
 func _on_CharacterStats_health_changed(new_value: int, _old_value: int) -> void:
-	_health_bar.set_value_no_signal(new_value)
+	set_cur_health(new_value)
 
 
 # Updates the max  health value of the label.
 func _on_CharacterStats_max_health_changed(new_value: int) -> void:
-	_health_bar.max_value = new_value
+	set_max_health(new_value)
 
 
 # Updates the initiative label.
