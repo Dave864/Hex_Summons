4cce73016ca50ec2016f201061eb3fcfe9ae5a5c
Dave864 <dmacpherson864@gmail.com>
2025-09-23 16:32:12 -0700

    p0.07-ui: Added new TextureRect in MovementButton to handle the selector image for the button. Adjusted the animation player logic in MovementButton to account for the change.

--- user_interface/encounter/PlayerOptionButton/movement_button.gd
+++ user_interface/encounter/PlayerOptionButton/movement_button.gd
@@ -11,14 +11,14 @@ onready var ap_icon: AnimationPlayer = $APIcon
 
 
 # Grabs the focus for the UI.
-func _on_MovementButton_mouse_entered():
+func _on_MovementButton_mouse_entered() -> void:
 	if disabled:
 		return
 	call_deferred("grab_focus")
 
 
 # Plays focus animation.
-func _on_MovementButton_focus_entered():
+func _on_MovementButton_focus_entered() -> void:
 	if pressed:
 		ap_focus.play("focus_selected")
 	else:
@@ -26,22 +26,22 @@ func _on_MovementButton_focus_entered():
 
 
 # Toggles the Movement button off when focus has shifted to a new Control item.
-func _on_MovementButton_focus_exited():
-	print("MovementButton focus exited")
+func _on_MovementButton_focus_exited() -> void:
+	pass
 	ap_focus.play("RESET")
 	if not pressed:
 		ap_icon.play("RESET")
 
 
 # Keeps the Movement button toggled on when it is in focus.
-func _on_MovementButton_toggled(button_pressed: bool):
+func _on_MovementButton_toggled(button_pressed: bool) -> void:
 	if button_pressed:
+		ap_focus.play("focus_selected")
 		ap_icon.play("selected_start")
 		yield(ap_icon, "animation_finished")
 		ap_icon.play("selected_loop")
 	elif has_focus():
 		set_pressed_no_signal(true)
-		ap_focus.play("focus_selected")
 	else:
-		ap_icon.play("RESET")
 		ap_focus.play("RESET")
+		ap_icon.play("RESET")
