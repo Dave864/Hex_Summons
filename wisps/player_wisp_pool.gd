cfd85fe37b03dd350cdf73bcb946eefd3f39d4d4
Dave864 <dmacpherson864@gmail.com>
2025-10-07 23:57:04 -0700

    p0.07-spells: Added logic to PlayerWispPool for activating and deactivating wisps in a player's pool.

--- wisps/player_wisp_pool.gd
+++ wisps/player_wisp_pool.gd
@@ -4,6 +4,7 @@ extends WispPool
 Tracks the wisp states for a Player character.
 """
 
+
 # Tracks which wisps are "active", i.e. which wisps are available to be used
 # for actions.
 var earth: Dictionary = {}
@@ -15,13 +16,47 @@ var wind: Dictionary = {}
 # Updates the state of the specified wisp to "active"
 func set_active(wisp_key: int) -> void:
 	if earth.has(wisp_key):
-		pass
+		earth[wisp_key] = true
+		_active_count[Constants.Element.EARTH] += 1
 	if fire.has(wisp_key):
-		pass
+		fire[wisp_key] = true
+		_active_count[Constants.Element.FIRE] += 1
 	if water.has(wisp_key):
-		pass
+		water[wisp_key] = true
+		_active_count[Constants.Element.WATER] += 1
 	if wind.has(wisp_key):
-		pass
+		wind[wisp_key] = true
+		_active_count[Constants.Element.WIND] += 1
+
+
+# Gets the keys for the wisps that are used to pay for the specified element.
+# Deactivates the wisps that are spent. Returns an empty array if no wisps
+# are available for the given element.
+func pay_for_element(element: int) -> Array:
+	match element:
+		Constants.Element.EARTH:
+			var id: int = _deactivate_first_active(earth, element)
+			if id < 0:
+				return [id]
+		Constants.Element.FIRE:
+			var id: int = _deactivate_first_active(fire, element)
+			if id < 0:
+				return [id]
+		Constants.Element.WATER:
+			var id: int = _deactivate_first_active(water, element)
+			if id < 0:
+				return [id]
+		Constants.Element.WIND:
+			var id: int = _deactivate_first_active(wind, element)
+			if id < 0:
+				return [id]
+		Constants.Element.LIGHT:
+			var elems: Array = ElementalPolarity.get_light_elements()
+			return _deactivate_polar_active(elems[0], elems[1])
+		Constants.Element.DARK:
+			var elems: Array = ElementalPolarity.get_dark_elements()
+			return _deactivate_polar_active(elems[0], elems[1])
+	return []
 
 
 # Called when the node enters the scene tree for the first time.
@@ -34,3 +69,42 @@ func _ready():
 		_active_count[Constants.Element.WATER] += 1 if is_active else 0
 	for is_active in wind.values():
 		_active_count[Constants.Element.WIND] += 1 if is_active else 0
+
+
+# Helper function for pay_for_element. Deactivates the first active wisp in the
+# given category. Returns the key of said wisp, if any. Returns -1 if none are
+# found.
+func _deactivate_first_active(wisps: Dictionary, element: int) -> int:
+	for id in wisps.keys():
+		if wisps[id]:
+			wisps[id] = false
+			_active_count[element] -= 1
+			return id
+	return -1
+
+
+# Helper function for pay_for_element. Deactivates the first active wisps for the
+# relevant polar elements. Return an empty array if not enough elements are
+# active.
+func _deactivate_polar_active(elem_1: int, elem_2: int) -> Array:
+	if _active_count[elem_1] == 0 or _active_count[elem_2] == 0:
+		return []
+	return [
+		_deactivate_first_active(_get_element_tracker(elem_1), elem_1),
+		_deactivate_first_active(_get_element_tracker(elem_2), elem_2)
+	]
+
+
+# Gets the tracker Dictionary for the given element. Returns an empty Dictionary
+# if no corresponding tracker is found.
+func _get_element_tracker(element: int) -> Dictionary:
+	match element:
+		Constants.Element.EARTH:
+			return earth
+		Constants.Element.FIRE:
+			return fire
+		Constants.Element.WATER:
+			return water
+		Constants.Element.WIND:
+			return wind
+	return {}
