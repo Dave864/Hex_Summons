3b324735eeefa2ccf78ff0126b2530fb18d7e6e4
Dave864 <dmacpherson864@gmail.com>
2025-10-09 22:48:16 -0700

    p0.07-spells: Implemented logic for updating the count labels when the elemental polarity changes.

--- user_interface/encounter/SummonPool/wisp_pool_ui.gd
+++ user_interface/encounter/SummonPool/wisp_pool_ui.gd
@@ -135,7 +135,26 @@ func _on_ElementalPolarity_polarity_changed() -> void:
 
 # Update the count label for the pinged element.
 func _on_CoreElementIcon_element_ping(core_elem: int) -> void:
-	pass
+	if (
+		_polarities[LIGHT][0] == core_elem
+		and String(pool.active_element_count(core_elem)) != light_elem_1_label.text
+	):
+		light_elem_1_label.text = String(pool.active_element_count(core_elem))
+	elif (
+		_polarities[LIGHT][1] == core_elem
+		and String(pool.active_element_count(core_elem)) != light_elem_2_label.text
+	):
+		light_elem_2_label.text = String(pool.active_element_count(core_elem))
+	elif (
+		_polarities[DARK][0] == core_elem
+		and String(pool.active_element_count(core_elem)) != dark_elem_1_label.text
+	):
+		dark_elem_1_label.text = String(pool.active_element_count(core_elem))
+	elif (
+		_polarities[DARK][1] == core_elem
+		and String(pool.active_element_count(core_elem)) != dark_elem_2_label.text
+	):
+		dark_elem_2_label.text = String(pool.active_element_count(core_elem))
 
 
 # Update the count label for the pinged element
