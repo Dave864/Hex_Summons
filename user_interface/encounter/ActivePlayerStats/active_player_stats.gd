5750a1a714f34be58b2b6d9f69fe92a3ee743ea5
Dave864 <dmacpherson864@gmail.com>
2025-10-10 21:36:55 -0700

    p0.07-spells: Added logic to set the portrait to default icon.

--- user_interface/encounter/ActivePlayerStats/active_player_stats.gd
+++ user_interface/encounter/ActivePlayerStats/active_player_stats.gd
@@ -6,6 +6,10 @@ currently active player character.
 """
 
 
+const PORTRAIT_SIZE: Vector2 = Vector2(46, 46)
+
+var _default_portrait: Texture = preload("res://art/icon.png")
+
 onready var _player_portrait: TextureRect = $PlayerWispPool/PlayerPortrait
 onready var _health_bar: TextureProgress = $HealthBar
 onready var _health_label: Label = $HealthNumberLabel
@@ -17,7 +21,13 @@ onready var _wisp_pool: PlayerWispPoolUI = $PlayerWispPool
 func set_stats(player: PlayerCharacter) -> void:
 	var cur_health: int = player.stats.get_stat(Stat.Type.CUR_HEALTH)
 	var max_health: int = player.stats.get_stat(Stat.Type.MAX_HEALTH)
+	"""
+	TODO: Update logic to load the active portrait if available. Update logic
+	to set the player wisp pool.
+	"""
 #	_wisp_pool.set_wisp_pool(player.wisp_pool)
+	_player_portrait.texture = _default_portrait
+	_player_portrait.rect_size = PORTRAIT_SIZE
 	_health_bar.max_value = max_health
 	_health_bar.value = cur_health
 	_health_label.text = "{0}/{1}".format([cur_health, max_health])
