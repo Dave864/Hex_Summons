474bcde98034cd508a2b91f3318c44ec51eb56ff
Dave864 <dmacpherson864@gmail.com>
2025-10-10 21:30:55 -0700

    p0.07-spells: Added logic to populate the health bar with the correct values to ActivePlayerStats.

--- user_interface/encounter/ActivePlayerStats/active_player_stats.gd
+++ user_interface/encounter/ActivePlayerStats/active_player_stats.gd
@@ -7,6 +7,7 @@ currently active player character.
 
 
 onready var _player_portrait: TextureRect = $PlayerWispPool/PlayerPortrait
+onready var _health_bar: TextureProgress = $HealthBar
 onready var _health_label: Label = $HealthNumberLabel
 onready var _name_label: Label = $NameLabel
 onready var _wisp_pool: PlayerWispPoolUI = $PlayerWispPool
@@ -14,12 +15,10 @@ onready var _wisp_pool: PlayerWispPoolUI = $PlayerWispPool
 
 # Populate the display elements with the player stats.
 func set_stats(player: PlayerCharacter) -> void:
+	var cur_health: int = player.stats.get_stat(Stat.Type.CUR_HEALTH)
+	var max_health: int = player.stats.get_stat(Stat.Type.MAX_HEALTH)
 #	_wisp_pool.set_wisp_pool(player.wisp_pool)
-	_health_label.text = (
-			"%d/%d" % \
-			[
-				player.stats.get_stat(Stat.Type.CUR_HEALTH),
-				player.stats.get_stat(Stat.Type.MAX_HEALTH)
-			]
-	)
+	_health_bar.max_value = max_health
+	_health_bar.value = cur_health
+	_health_label.text = "{0}/{1}".format([cur_health, max_health])
 	_name_label.text = player.name
