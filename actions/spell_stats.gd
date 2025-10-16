2ea3313a45cdc2545a79538e1292de1cbf0e75f6
Dave864 <dmacpherson864@gmail.com>
2025-10-16 15:20:29 -0700

    p0.07-party: Created resource scripts for player techniques and spells.

---
+++ actions/spell_stats.gd
@@ -0,0 +1,66 @@
+class_name SpellStats
+extends ActionStats
+"""
+Describes a spell. Spells are actions with a wisp requirement and cost.
+"""
+
+
+export(int, 0, 4) var earth_req = 0
+export(int, 0, 4) var earth_cost = 0
+export(int, 0, 4) var fire_req = 0
+export(int, 0, 4) var fire_cost = 0
+export(int, 0, 4) var water_req = 0
+export(int, 0, 4) var water_cost = 0
+export(int, 0, 4) var wind_req = 0
+export(int, 0, 4) var wind_cost = 0
+export(int, 0, 4) var light_req = 0
+export(int, 0, 4) var light_cost = 0
+export(int, 0, 4) var dark_req = 0
+export(int, 0, 4) var dark_cost = 0
+
+var summary: Dictionary = _get_costs()
+
+
+# Checks if the wisp pool meets the requirements described by these stats.
+func is_met(wisp_pool: WispPool) -> bool:
+	return (
+		wisp_pool.active_earth_count() >= earth_req
+		and wisp_pool.active_fire_count() >= fire_req
+		and wisp_pool.active_water_count() >= water_req
+		and wisp_pool.active_wind_count() >= wind_req
+		and wisp_pool.active_light_count() >= light_req
+		and wisp_pool.active_dark_count() >= dark_req
+	)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_check_for_required_parameters()
+
+
+# Gets the elements with cost values.
+func _get_costs() -> Dictionary:
+	var element_costs: Dictionary = {}
+	if earth_cost > 0:
+		element_costs[Constants.Element.EARTH] = earth_cost
+	if fire_cost > 0:
+			element_costs[Constants.Element.FIRE] = fire_cost
+	if water_cost > 0:
+			element_costs[Constants.Element.WATER] = water_cost
+	if wind_cost > 0:
+			element_costs[Constants.Element.WIND] = wind_cost
+	if light_cost > 0:
+			element_costs[Constants.Element.LIGHT] = light_cost
+	if dark_cost > 0:
+			element_costs[Constants.Element.DARK] = dark_cost
+	return element_costs
+
+
+# Check that all required parameters are set and valid.
+func _check_for_required_parameters() -> void:
+	assert(earth_req >= earth_cost, "Earth requirement less than cost")
+	assert(fire_req >= fire_cost, "Fire requirement less than cost")
+	assert(water_req >= water_cost, "Water requirement less than cost")
+	assert(wind_req >= wind_cost, "Wind requirement less than cost")
+	assert(light_req >= light_cost, "Light requirement less than cost")
+	assert(dark_req >= dark_cost, "Dark requirement less than cost")
---
+++ actions/technique_stats.gd
@@ -0,0 +1,37 @@
+class_name TechniqueStats
+extends ActionStats
+"""
+Describes a technique. Techniques are actions with a cooldown.
+"""
+
+
+export(int, 0, 10) var cooldown = 0
+
+var _countdown: int = 0 setget , get_countdown
+
+
+# Gets the current value of the countdown.
+func get_countdown() -> int:
+	return _countdown
+
+
+# Checks if the countdown is active.
+func is_active() -> bool:
+	return _countdown > 0
+
+
+# Starts the cooldown countdown.
+func start_countdown() -> void:
+	_countdown = cooldown
+
+
+# Starts the cooldown countdown. Used when the countdown is started during the
+# character's turn. Need to add one extra count so that cooldown duration
+# matches turn_count.
+func start_countdown_on_turn() -> void:
+	_countdown = cooldown + 1
+
+
+# Decrement the countdown when a character turn ends.
+func _on_Character_turn_ended() -> void:
+	_countdown = 0 if _countdown == 0 else _countdown - 1
