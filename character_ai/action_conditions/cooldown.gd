6166a24640897cfe5059887ab64536dde2dbce5a
Dave864 <dmacpherson864@gmail.com>
2025-10-31 20:01:03 -0700

    p0.07-spells: Fixed init function for Cooldown so that it works properly. Actions with Cooldowns were being set as active initially, and were not being set to active when starting the countdown.

--- character_ai/action_conditions/cooldown.gd
+++ character_ai/action_conditions/cooldown.gd
@@ -1,43 +1,46 @@
 class_name Cooldown
 extends Node
-"""
-A timer that updates whenever a character's turn ends.
-"""
+## A timer that updates whenever a character's turn ends.
 
 
+## The number of turns the cooldown is active for.
 @export var turn_count = 0 # (int, 0, 10)
 
+## Tracks the countdown value when the cooldown is active. A value of 0 indicates
+## the countdown has completed.
 var _countdown: int = 0: get = get_countdown
 
 
-# Gets the current value of the countdown.
+## Gets the current value of the countdown.
 func get_countdown() -> int:
 	return _countdown
 
 
-# Checks if the countdown is active.
+## Checks if the countdown is active.
 func is_active() -> bool:
 	return _countdown > 0
 
 
-# Starts the cooldown countdown.
+## Starts the cooldown countdown.
 func start_countdown() -> void:
 	_countdown = turn_count
 
 
-# Starts the cooldown countdown. Used when the countdown is started during the
-# character's turn. Need to add one extra count so that cooldown duration
-# matches turn_count.
+## Starts the cooldown countdown. Used when the countdown is started during the
+## character's turn. Need to add one extra count so that cooldown duration
+## matches turn_count.
 func start_countdown_on_turn() -> void:
 	_countdown = turn_count + 1
 
 
-# Creates a new Cooldown node with a specific cooldown_value value.
+## Creates a new Cooldown node with a specific cooldown_value value. The Cooldown
+## is inactive upon creation.
 func _init(cooldown_value: int = 0) -> void:
 	name = "Cooldown"
-	_countdown = cooldown_value
+	turn_count = cooldown_value
+	_countdown = 0
 
 
-# Decrement the countdown when a character turn ends.
+## Decrement the countdown when a character turn ends.
 func _on_Character_turn_ended() -> void:
 	_countdown = 0 if _countdown == 0 else _countdown - 1
