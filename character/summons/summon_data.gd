5a99cb3e775551fc1a46cb088e6478bc8f43dc40
Dave864 <dmacpherson864@gmail.com>
2025-12-03 22:31:52 -0800

    p0.08-summon: Created two test summons with actions. Created new action, SuperNova. Refactored SummonData to use export_range annotation when appropriate.

--- character/summons/summon_data.gd
+++ character/summons/summon_data.gd
@@ -6,16 +6,31 @@ extends Resource
 ## image data, stat multipliers, wisp requirements, and available actions.
 
 
+@export var name: String = ""
 @export_group("Images")
 @export var portrait: Texture2D = null
 @export var battle_sprite: Texture2D = null
+@export_group("Summoner Stat Multipliers")
+@export_range(0, 10) var movement: int = 1
+@export_range(0.0, 5.0, 0.1) var attack: float = 1.0
+@export_range(0.0, 5.0, 0.1) var defense: float = 1.0
+@export_subgroup("Magic", "magic_")
+@export_range(0.0, 5.0, 0.1) var magic_earth: float = 1.0
+@export_range(0.0, 5.0, 0.1) var magic_fire: float = 1.0
+@export_range(0.0, 5.0, 0.1) var magic_water: float = 1.0
+@export_range(0.0, 5.0, 0.1) var magic_wind: float = 1.0
+@export_subgroup("Resistance", "res_")
+@export_range(0.0, 5.0, 0.1) var res_earth: float = 1.0
+@export_range(0.0, 5.0, 0.1) var res_fire: float = 1.0
+@export_range(0.0, 5.0, 0.1) var res_water: float = 1.0
+@export_range(0.0, 5.0, 0.1) var res_wind: float = 1.0
 @export_group("Wisp Requirement and Cost")
-@export var earth_req = 0 # (int, 0, 4)
-@export var fire_req = 0 # (int, 0, 4)
-@export var water_req = 0 # (int, 0, 4)
-@export var wind_req = 0 # (int, 0, 4)
-@export var light_req = 0 # (int, 0, 4)
-@export var dark_req = 0 # (int, 0, 4)
+@export_range(0, 4) var earth_req: int = 0
+@export_range(0, 4) var fire_req: int = 0
+@export_range(0, 4) var water_req: int = 0
+@export_range(0, 4) var wind_req: int = 0
+@export_range(0, 4) var light_req: int = 0
+@export_range(0, 4) var dark_req: int = 0
 @export_group("Actions")
 @export var summon_action: ActionStats = null
 @export var turn_actions: Array[SpellStats] = []
