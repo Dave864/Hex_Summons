b5a5293299ba1f349ea439609def449fdb94da2e
Dave864 <dmacpherson864@gmail.com>
2025-12-20 17:29:51 -0800

    p0.08-static_typing: Refactored ElementalAlignment autoload.
    Changed how ElementalAlignment tracks which core elements are aligned with which alignment elements to resolve issues with nested containers (Dictionary, Array).

--- autoloads/elemental_alignment.gd
+++ autoloads/elemental_alignment.gd
@@ -8,13 +8,15 @@ extends Node
 ## Indicates that the alignment of core elements has changed.
 signal alignment_changed()
 
+## Shorthand for the LIGHT elemental alignment.
 const LIGHT: Element.Alignment = Element.Alignment.LIGHT
+## Shorthand for the DARK elemental alignment.
 const DARK: Element.Alignment = Element.Alignment.DARK
 
-var _alignments: Dictionary[Element.Alignment, Array] = {
-	LIGHT: [-1, -1],
-	DARK: [-1, -1],
-}
+## The elements that are aligned with Light.
+var _light_elements: Array[Element.Core] = [Element.Core.FIRE, Element.Core.WIND]
+## The elements that are aligned with Dark.
+var _dark_elements: Array[Element.Core] = [Element.Core.EARTH, Element.Core.WATER]
 
 
 ## Called when the node enters the scene tree for the first time.
@@ -23,7 +25,7 @@ func _ready() -> void:
 
 
 ## Swap the alignments of the given elements.
-func swap_elements(element_1: int, element_2: int) -> void:
+func swap_elements(element_1: Element.Core, element_2: Element.Core) -> void:
 	if (
 		not _is_valid_core_element(element_1)
 		or not _is_valid_core_element(element_2)
@@ -32,8 +34,14 @@ func swap_elements(element_1: int, element_2: int) -> void:
 		return
 	var element_1_details: Array[int] = _get_alignment_and_index(element_1)
 	var element_2_details: Array[int] = _get_alignment_and_index(element_2)
-	_alignments[element_1_details[0]][element_1_details[1]] = element_2
-	_alignments[element_2_details[0]][element_2_details[1]] = element_1
+	var element_1_alignment: Array[Element.Core] = (
+		_light_elements if element_1_details[0] == LIGHT else _dark_elements
+	)
+	var element_2_alignment: Array[Element.Core] = (
+		_light_elements if element_2_details[0] == LIGHT else _dark_elements
+	)
+	element_1_alignment[element_1_details[1]] = element_2
+	element_2_alignment[element_1_details[1]] = element_1
 	emit_signal("alignment_changed")
 
 
@@ -62,11 +70,11 @@ func invert_right_alignments() -> void:
 ## L: [0, 1] => [1, 3]
 ## D: [2, 3] => [0, 2]
 func shift_alignments_ccw() -> void:
-	var first_light_element: int = _alignments[LIGHT][0]
-	_alignments[LIGHT][0] = _alignments[LIGHT][1]
-	_alignments[LIGHT][1] = _alignments[DARK][1]
-	_alignments[DARK][1] = _alignments[DARK][0]
-	_alignments[DARK][0] = first_light_element
+	var first_light_element: Element.Core = _light_elements[0]
+	_light_elements[0] = _light_elements[1]
+	_light_elements[1] = _dark_elements[1]
+	_dark_elements[1] = _dark_elements[0]
+	_dark_elements[0] = first_light_element
 	emit_signal("alignment_changed")
 
 
@@ -74,16 +82,19 @@ func shift_alignments_ccw() -> void:
 ## L: [0, 1] => [2, 0]
 ## D: [2, 3] => [3, 1]
 func shift_alignments_cw() -> void:
-	var first_light_element: int = _alignments[LIGHT][0]
-	_alignments[LIGHT][0] = _alignments[DARK][0]
-	_alignments[DARK][0] = _alignments[DARK][1]
-	_alignments[DARK][1] = _alignments[LIGHT][1]
-	_alignments[LIGHT][1] = first_light_element
+	var first_light_element: Element.Core = _light_elements[0]
+	_light_elements[0] = _dark_elements[0]
+	_dark_elements[0] = _dark_elements[1]
+	_dark_elements[1] = _light_elements[1]
+	_light_elements[1] = first_light_element
 	emit_signal("alignment_changed")
 
 
 ## Changes the elements that are of the Light alignment.
-func set_elements_to_light(element_1: int, element_2: int) -> void:
+func set_elements_to_light(
+	element_1: Element.Core,
+	element_2: Element.Core
+) -> void:
 	if (
 		not _is_valid_core_element(element_1)
 		or not _is_valid_core_element(element_2)
@@ -108,36 +119,31 @@ func set_elements_to_dark(element_1: int, element_2: int) -> void:
 
 ## Get the elements of the Light alignment.
 func get_light_elements() -> Array[Element.Core]:
-	return _alignments[LIGHT]
+	return _light_elements
 
 
 ## Get the elements of the Dark alignment.
 func get_dark_elements() -> Array[Element.Core]:
-	return _alignments[DARK]
+	return _dark_elements
 
 
 ## Gets the alignment of the given element, as defined by the Constants enum,
 ## AlignmentElement.
-func get_alignment(element: int) -> int:
+func get_alignment(element: Element.Type) -> Element.Alignment:
 	if element == LIGHT or element == DARK:
-		return element
-	elif element == _alignments[LIGHT][0] or element == _alignments[LIGHT][1]:
+		return element as Element.Alignment
+	elif element == _light_elements[0] or element == _light_elements[1]:
 		return LIGHT
-	elif element == _alignments[DARK][0] or element == _alignments[DARK][1]:
-		return DARK
 	else:
-		return -1
+		return DARK
 
 
 ## Sets the two elements to the specified alignment.
 func _set_elements_to_alignment(
-	target_alignment: int,
-	element_1: int,
-	element_2: int
+	target_alignment: Element.Alignment,
+	element_1: Element.Core,
+	element_2: Element.Core
 ) -> void:
-	if target_alignment in Element.Alignment.keys():
-		printerr("target_alignment is not a 'polar' element.")
-		return
 	var inverse_alignment: int = LIGHT if target_alignment == DARK else DARK
 	var element_1_details: Array[int] = _get_alignment_and_index(element_1)
 	var element_2_details: Array[int] = _get_alignment_and_index(element_2)
@@ -170,25 +176,30 @@ func _set_elements_to_alignment(
 				shift_alignments_cw()
 			else:
 				shift_alignments_ccw()
-	_alignments[target_alignment][0] = element_1
-	_alignments[target_alignment][1] = element_2
+	if target_alignment == LIGHT:
+		_light_elements[0] = element_1
+		_light_elements[1] = element_2
+	else:
+		_dark_elements[0] = element_1
+		_dark_elements[1] = element_2
 
 
 ## Gets the alignment and index of a given element. Returns the details in an
 ## array: [alignment, index]
-func _get_alignment_and_index(element: int) -> Array[int]:
-	for p: Element.Alignment in _alignments:
-		for i: int in len(p):
-			if _alignments[p][i] == element:
-				return [p, i]
+func _get_alignment_and_index(element: Element.Core) -> Array[int]:
+	for i: int in 2:
+		if _light_elements[i] == element:
+			return [LIGHT, i]
+		elif _dark_elements[i] == element:
+			return [DARK, i]
 	return [-1, -1]
 
 
 ## Swap the elements at the given index for each alignment.
 func _swap_alignments_at_index(index: int) -> void:
-	var light_element: int = _alignments[LIGHT][index]
-	_alignments[LIGHT][index] = _alignments[DARK][index]
-	_alignments[DARK][index] = light_element
+	var light_element: Element.Core = _light_elements[index]
+	_light_elements[index] = _dark_elements[index]
+	_dark_elements[index] = light_element
 
 
 ## Checks if a given value corresponds to an elemental type.
@@ -198,7 +209,7 @@ func _is_valid_core_element(element: int) -> bool:
 
 ## Sets Fire and Wind to Light. Sets Earth and Water to Dark.
 func _set_to_default() -> void:
-	_alignments[LIGHT][0] = Element.Core.FIRE
-	_alignments[LIGHT][1] = Element.Core.WIND
-	_alignments[DARK][0] = Element.Core.EARTH
-	_alignments[DARK][1] = Element.Core.WATER
+	_light_elements[0] = Element.Core.FIRE
+	_light_elements[1] = Element.Core.WIND
+	_dark_elements[0] = Element.Core.EARTH
+	_dark_elements[1] = Element.Core.WATER
