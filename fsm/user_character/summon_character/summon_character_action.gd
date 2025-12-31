5b99148417153250a45b9919d06042a33530bb20
Dave864 <dmacpherson864@gmail.com>
2025-12-30 20:12:16 -0800

    p0.08-summon: Fixed improper reference to summon character's wisp pool.

--- fsm/user_character/summon_character/summon_character_action.gd
+++ fsm/user_character/summon_character/summon_character_action.gd
@@ -13,6 +13,6 @@ func _spend_resource_for_action(action: Action) -> void:
 	var wisp_cost: WispCost = action.get_node_or_null("WispCost")
 	if wisp_cost != null:
 		WispController.pay_cost_from_active_summon(
-				character.wisp_pool,
+				character.summon_wisp_pool,
 				wisp_cost
 		)
