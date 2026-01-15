6241e35924e863eb4ee379caa2e5dd68bd98c9dc
Dave864 <dmacpherson864@gmail.com>
2026-01-14 23:47:34 -0800

    p0.09-action_effects: Created new script, effects_list.
    This script describes a new node, EffectsList, that will track and manage Effects. This is the start of a refactor for the action effects system.

---
+++ effects/effects_list.gd
@@ -0,0 +1,41 @@
+class_name EffectsList
+extends Node
+## Tracks all effects for an action.
+##
+## This node will track all children that are an Effect node. Handles
+## initialization and updating of action potency and source character stat
+## references for tracked effects.
+
+
+## The list of effects.
+var _effects: Array[Effect] = []
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for child_node: Node in get_children():
+		if child_node is Effect:
+			_effects.append(child_node as Effect)
+	_check_for_required_parameters()
+
+
+## Returns the effects tracked.
+func get_effects() -> Array[Effect]:
+	return _effects
+
+
+## Updates the action_potency reference for this effect.
+func set_action_potency(action_potency: Potency) -> void:
+	for effect: Effect in _effects:
+		effect.set_action_potency(action_potency)
+
+
+## Updates the source character stats of this effect.
+func set_source_stats(new_source: StatModifiers) -> void:
+	for effect: Effect in _effects:
+		effect.set_source_stats(new_source)
+
+
+## Check that all required parameters are set.
+func _check_for_required_parameters() -> void:
+	assert(_effects.size() > 0, "Error: No effects in EffectList.")
