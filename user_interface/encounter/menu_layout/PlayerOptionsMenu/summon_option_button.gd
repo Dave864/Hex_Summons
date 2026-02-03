583222b172d5966f18971e8a10c8da66e04bf735
Dave864 <dmacpherson864@gmail.com>
2026-02-02 22:10:01 -0800

    p0.09-menu_ui: Created script for SummonOptionButton.
    Summons use a different structure for their spawn actions and need their own logic to account for it.

---
+++ user_interface/encounter/menu_layout/PlayerOptionsMenu/summon_option_button.gd
@@ -0,0 +1,21 @@
+class_name SummonOptionButton
+extends ActionOptionButton
+## A button that represents a summon option.
+##
+## Tracks the summon that this button is for. Passes along its spawn action data
+## when this button is pressed.
+
+
+## Creates a button with the specified summon, getting its details from the
+## summon manager.
+func _init(summon_name: String, spawn_action: Action) -> void:
+	_action = spawn_action
+	name = summon_name
+	text = name
+	alignment = HORIZONTAL_ALIGNMENT_CENTER
+	flat = true
+	connect("pressed", Callable(self, "_on_ActionOptionButton_pressed"))
+	connect(
+			"focus_entered",
+			Callable(self, "_on_ActionOptionButton_focus_entered")
+	)
