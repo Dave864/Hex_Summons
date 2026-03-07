8cd4bddebc6521ea2cb5d72568bf9f77dc88e60a
Dave864 <dmacpherson864@gmail.com>
2026-03-07 01:49:14 -0800

    p0.09-gamepad: Adjusted how the selector is handled for positional actions.
    The selector has gone back to being static for actions centered on the caster. The selector is able to move freely across the map, but the effect range will not show unless the selector is on a target tile.

--- fsm/selection_tracker/selection_tracker_positional_action.gd
+++ fsm/selection_tracker/selection_tracker_positional_action.gd
@@ -15,10 +15,11 @@ func enter(msg: Dictionary[Variant, Variant] = {}) -> void:
 	super.enter(msg)
 	var action: Action = s_tracker.get_focus_action()
 	if action.is_centered_on_caster():
+		s_tracker.emit_camera_focus_locked()
+		selector.tile_hovered = hex_map.get_tile_at(s_tracker.player_index)
 		_update_emission_tile(s_tracker.player_index)
 	else:
 		_place_closest_to_target()
-	s_tracker.emit_new_focus_point(action.get_emission_pos())
 
 
 ## Updates the emission tile for the focused action.
@@ -37,27 +38,35 @@ func _update_selection(map_tile: MapTile) -> void:
 	assert(map_tile != null, "SelectAction given a null MapTile.")
 	var action: Action = s_tracker.get_focus_action()
 	MouseHandler.update_mouse_tracker_3d(map_tile.get_character_position())
-	selector.tile_hovered = map_tile
-	# Actions centered on caster should not update the selection. Also, actions
-	# with dead range need to display the character tile highlight, but not
-	# allow emission from character position.
-	if (
-		action.is_centered_on_caster()
-		or !map_tile.is_active()
-		or (
-			action.stats.dead_range.get_reach() > 0
-			and map_tile.map_coordinate.get_tile_index() == s_tracker.player_index
-		)
-	):
+	# Actions centered on caster should not update the selection.
+	if action.is_centered_on_caster():
 		return
+	action.set_emission_map_index(map_tile.map_coordinate.get_tile_index())
+	action.set_emission_pos(map_tile.get_character_position())
 	if _is_target_tile(map_tile):
+		selector.tile_hovered = map_tile
 		if InputController.source_is_gamepad():
 			s_tracker.emit_new_focus_point(map_tile.get_character_position())
-		action.set_emission_map_index(map_tile.map_coordinate.get_tile_index())
-		action.set_emission_pos(map_tile.get_character_position())
 		_highlight_effect_range()
 	else:
-		_place_closest_to_tile(map_tile.map_coordinate.get_tile_index())
+		# Turn off previous selector indicators to indicate a new tile is being
+		# hovered over.
+		if _is_target_tile(selector.tile_hovered):
+			s_tracker.clear_indicators()
+		else:
+			selector.tile_hovered.set_selector_type(HexHighlighter.Option.NONE)
+		selector.tile_hovered = map_tile
+		if InputController.source_is_gamepad():
+			s_tracker.emit_new_focus_point(map_tile.get_character_position())
+		map_tile.set_selector_type(HexHighlighter.Option.GRAY)
+
+
+## Determines if the selector is able to move to the adjacent tile in the
+## given direction and does so if able.
+func _resolve_joystick_for_area(dir: HexUtil.HexDirection) -> void:
+	var adjacent_tile: MapTile = selector.tile_hovered.get_adjacent_tile(dir)
+	if adjacent_tile != null:
+		_update_selection(adjacent_tile)
 
 
 ## Places the effect emission so that the effect area highlights the closest
@@ -90,10 +99,14 @@ func _place_closest_to_target() -> void:
 	if closest_index < 0:
 		s_tracker.clear_indicators()
 		return
+	# Set selector to 'NONE' to account for instances where selector was
+	# outside of source range.
+	selector.tile_hovered.set_selector_type(HexHighlighter.Option.NONE)
 	selector.tile_hovered = hex_map.get_tile_at(closest_index)
 	action.set_emission_map_index(closest_index)
 	action.set_emission_pos(selector.tile_hovered.get_character_position())
 	_highlight_effect_range()
+	s_tracker.emit_new_focus_point(action.get_emission_pos())
 
 
 ## Places the effect emission so that it is on the source tile closest to the
