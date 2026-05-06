8cf5cfe6d68f3ab7bb347de14236907f893c4be6
Dave864 <dmacpherson864@gmail.com>
2026-05-06 01:36:27 -0700

    p0.12-overworld: Created initial movement script for OverworldAvatar, adding new movement input.
    Default movement script uses UI input, so dedicated input is needed.

---
+++ overworld/overworld_avatar/overworld_avatar.gd
@@ -0,0 +1,32 @@
+class_name OverworldAvatar
+extends CharacterBody3D
+## Represents the player character in the overworld scene.
+
+
+## The speed the avatar moves at.
+const SPEED = 5.0
+
+
+## Processes the physics of moving around.
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector(
+			"player_move_l",
+			"player_move_r",
+			"player_move_u",
+			"player_move_d"
+	)
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
