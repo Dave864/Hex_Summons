b0912c471de0a18e809b0748a088c4312ef615dc
Dave864 <dmacpherson864@gmail.com>
2026-05-10 20:08:17 -0700

    p0.12-encounter_spawn: Updated signal connections to be handled in script.
    When loaded as part of a HexMap, signals within MapTile would attempt to be
    reconnected when done so via in editor interface. Also updated game name.

--- hex_map/MapTile/scripts/map_tile.gd
+++ hex_map/MapTile/scripts/map_tile.gd
@@ -53,6 +53,7 @@ var _texture_options: Array[Texture2D]
 
 
 func _ready() -> void:
+	_connect_signals()
 	set_height(height)
 	$DebugLabel.update_label_display(height)
 
@@ -164,6 +165,31 @@ func is_active() -> bool:
 ## the tile.
 func get_character_position() -> Vector3:
 	return map_coordinate.global_position
+
+
+## Connects the signals for this object.
+func _connect_signals() -> void:
+	var height_changed_mouse_blocker_callable := Callable(
+			$MouseBlocker/ScalingHexCollider,
+			"_on_HeightSource_height_changed"
+	)
+	if not is_connected("height_changed", height_changed_mouse_blocker_callable):
+		connect("height_changed", height_changed_mouse_blocker_callable)
+	var height_changed_tile_mesh_callable := Callable(
+			tile_mesh,
+			"_on_HeightSource_height_changed"
+	)
+	if not is_connected("height_changed", height_changed_tile_mesh_callable):
+		connect("height_changed", height_changed_tile_mesh_callable)
+	var area_entered_callable := Callable(occupant, "_on_MapTile_area_entered")
+	if not is_connected("area_entered", area_entered_callable):
+		connect("area_entered", area_entered_callable)
+	var area_exited_callable := Callable(occupant, "_on_MapTile_area_exited")
+	if not is_connected("area_exited", area_exited_callable):
+		connect("area_exited", area_exited_callable)
+	var mouse_entered_callable := Callable(self, "_on_MapTile_mouse_entered")
+	if not is_connected("mouse_entered", mouse_entered_callable):
+		connect("mouse_entered", mouse_entered_callable)
 
 
 ## Update the position of the tile highlighters so that they are on top of
