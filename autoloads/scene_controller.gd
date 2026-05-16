e5ef2519d4a86923f57a97cc264e1751f79eef43
Dave864 <dmacpherson864@gmail.com>
2026-05-15 19:23:22 -0700

    p0.12-loading: Created initial logic for switching to a new scene.

--- autoloads/scene_controller.gd
+++ autoloads/scene_controller.gd
@@ -10,6 +10,14 @@ const OVERWORLD_SCENE_PATH := "res://overworld/Overworld.tscn"
 ## The last tracked position of the OverworldAvatar.
 var prior_avatar_position: Vector3 = Vector3.INF
 
+## The sceen currently active.
+var _current_scene: Node = null
+## Pre loaded loading screen.
+var _loading_screen: PackedScene = preload(
+		"res://user_interface/LoadingScreen/LoadingScreen.tscn"
+)
+## The path to the scene currently being loaded.
+var _load_scene_path: String = ""
 ## Reference to the OverworldAvatar.
 var _overworld_avatar: OverworldAvatar = null
 ## The squared distance the tracked avatar traveled in the last frame.
@@ -18,6 +26,11 @@ var _last_frame_distance: float = 0.0
 var _encounter_map_path: String = ""
 ## The paths to the enemies to use for the next encounter.
 var _encounter_enemy_paths: Array[String] = []
+
+
+## Obtains the current active scene.
+func _ready() -> void:
+	_current_scene = get_tree().current_scene
 
 
 ## Updates the last tracked position for the currently tracked OverworldAvatar.
@@ -48,3 +61,51 @@ func change_scene_to_encounter(
 ) -> void:
 	_encounter_map_path = map_path
 	_encounter_enemy_paths = enemy_paths
+	_load_scene.call_deferred(ENCOUNTER_SCENE_PATH)
+
+
+## Triggers a scene change to the Overworld scene.
+func change_scene_to_overworld() -> void:
+	_load_scene.call_deferred(OVERWORLD_SCENE_PATH)
+
+
+## Triggers a scene change to the specified path.
+func change_to_scene(scene_path: String) -> void:
+	_load_scene.call_deferred(scene_path)
+
+
+## Obtains the progress of any currently active scene switch as a value from 0
+## to 1.0.
+func get_load_percentage() -> float:
+	if _load_scene_path == "":
+		return 0.0
+	else:
+		var progress: Array[float] = []
+		ResourceLoader.load_threaded_get_status(_load_scene_path, progress)
+		return progress[0]
+
+
+## Loads the selected scene.
+func _load_scene(scene_path: String) -> void:
+	_load_scene_path = scene_path
+	var loading_screen: LoadingScreen = _loading_screen.instantiate()
+	get_tree().root.add_child(loading_screen)
+	_current_scene.queue_free()
+	ResourceLoader.load_threaded_request(scene_path)
+	while true:
+		match ResourceLoader.load_threaded_get_status(scene_path):
+			ResourceLoader.THREAD_LOAD_FAILED:
+				printerr("Scene at path {} failed to load.".format([scene_path]))
+				return
+			ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
+				printerr("Scene at path {} is invalid.".format([scene_path]))
+				return
+			ResourceLoader.THREAD_LOAD_LOADED:
+				break
+			_:
+				pass
+	var new_scene := ResourceLoader.load_threaded_get(scene_path)
+	_current_scene = new_scene.instantiate()
+	get_tree().root.add_child(_current_scene)
+	loading_screen.queue_free()
+	_load_scene_path = ""
