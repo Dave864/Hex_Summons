2ee5f21edd2c74f03072a32a0fe5169906b15481
Dave864 <dmacpherson864@gmail.com>
2026-06-14 02:35:27 -0700

    p0.12-encounter_spawn: Created SpawnAreaRect.
    This defines a rectangular area where enemies can spawn.

---
+++ overworld/encounter_zone/spawn_area_rect.gd
@@ -0,0 +1,60 @@
+@tool
+class_name SpawnAreaRect
+extends SpawnArea
+## Defines a rectangular area where EncounterSpawn may be instanced.
+
+
+## Indicates if the area is a square.
+@export var is_square := false:
+	set(value):
+		is_square = value
+		if is_square:
+			height = length
+## The length of the rectangle (x-axis). Dimension used when the area is a square.
+@export_range(0.01, 100.0, 0.01) var length := 1.0:
+	set(value):
+		length = value
+		if is_square:
+			height = length
+		if is_node_ready():
+			_update_debug_mesh()
+## The height of the reactangle (z-axis).
+@export_range(0.01, 100.0, 0.01) var height := 1.0:
+	set(value):
+		height = length if is_square else value
+		if is_node_ready():
+			_update_debug_mesh()
+
+## Half of the length.
+var _half_length: float:
+	get():
+		return length / 2
+## Half of the height.
+var _half_height: float:
+	get():
+		return height / 2
+
+
+## Updates the debug mesh to the current dimensions.
+func _update_debug_mesh() -> void:
+	if not Engine.is_editor_hint():
+		return
+	var rect_mesh := ImmediateMesh.new()
+	rect_mesh.surface_begin(Mesh.PRIMITIVE_LINE_STRIP)
+	rect_mesh.surface_add_vertex(Vector3(-_half_length, 0.0, _half_height))
+	rect_mesh.surface_add_vertex(Vector3(_half_length, 0.0, _half_height))
+	rect_mesh.surface_add_vertex(Vector3(_half_length, 0.0, -_half_height))
+	rect_mesh.surface_add_vertex(Vector3(-_half_length, 0.0, -_half_height))
+	rect_mesh.surface_add_vertex(Vector3(-_half_length, 0.0, _half_height))
+	rect_mesh.surface_end()
+	_debug_mesh.mesh = rect_mesh
+	_debug_mesh.set_surface_override_material(0, _debug_mesh_material())
+
+
+## Gets a random position in the defined spawn area plane.
+func _random_area_position() -> Vector3:
+	return Vector3(
+		randf_range(-_half_length, _half_length),
+		0.0,
+		randf_range(-_half_height, _half_height)
+	)
