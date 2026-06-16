30f1e6dd89e8c4bbab94730cf13fadbc16c0409d
Dave864 <dmacpherson864@gmail.com>
2026-06-16 02:01:40 -0700

    p0.12-spawn_behavior: Added node to track interest points.
    Interest points will be used to determine the travel destination of EncounterSpawn
    nodes.

--- overworld/encounter_zone/terrain_zone.gd
+++ overworld/encounter_zone/terrain_zone.gd
@@ -5,30 +5,30 @@ extends Area3D
 ## this area.
 
 
+## The name of the node that tracks the points of interest used to determine
+## where an EncounterSpawn can travel to.
+const INTEREST_POINTS_NAME := "InterestPoints"
 ## The name of the node that records the spawn areas assigned to this zone.
 const SPAWN_AREAS_NAME := "SpawnAreas"
 
 ## Files paths to the possible map selections for this zone.
 @export_dir var maps : Array[String]
 
+## The currently tracked points of interest.
+var points_of_interest : PackedVector3Array
+
+## The scene tree root.
+var _scene_root : Node:
+	get():
+		return get_tree().edited_scene_root
+
 
 ## Creates a new CollisionShape if none is present.
 func _ready() -> void:
-	if get_shape_owners().size() == 0:
-		var collision_shape := CollisionShape3D.new()
-		add_child(collision_shape)
-		if Engine.is_editor_hint():
-			collision_shape.set_owner(get_tree().edited_scene_root)
-		collision_shape.name = "CollisionShape3D"
-		collision_shape.debug_color = Color.YELLOW
-		collision_shape.debug_fill = false
-		collision_shape.shape = BoxShape3D.new()
-	if not has_node(SPAWN_AREAS_NAME):
-		var spawn_areas := Node3D.new()
-		add_child(spawn_areas)
-		if Engine.is_editor_hint():
-			spawn_areas.set_owner(get_tree().edited_scene_root)
-		spawn_areas.name = SPAWN_AREAS_NAME
+	_ready_collision_shape()
+	_ready_node(INTEREST_POINTS_NAME)
+	_get_points_of_interest()
+	_ready_node(SPAWN_AREAS_NAME)
 	_set_terrain_for_spawn_areas()
 
 
@@ -40,9 +40,43 @@ func _init() -> void:
 	set_collision_mask_value(Constants.PLAYER_LAYER, true)
 
 
+## Creates a new collision shape if none is already present.
+func _ready_collision_shape() -> void:
+	if get_shape_owners().size() == 0:
+		var collision_shape := CollisionShape3D.new()
+		add_child(collision_shape)
+		if Engine.is_editor_hint():
+			collision_shape.set_owner(_scene_root)
+		collision_shape.name = "CollisionShape3D"
+		collision_shape.debug_color = Color.YELLOW
+		collision_shape.debug_fill = false
+		collision_shape.shape = BoxShape3D.new()
+
+
+## Creates a Node of the specified name if it is not already present.
+func _ready_node(node_name: String) -> void:
+	if not has_node(node_name):
+		var node := Node.new()
+		add_child(node)
+		if Engine.is_editor_hint():
+			node.set_owner(_scene_root)
+		node.name = node_name
+
+
+## Gets the positions for the assinged points of interest.
+func _get_points_of_interest() -> void:
+	for point: Node in get_node(INTEREST_POINTS_NAME).get_children():
+		if point is Marker3D:
+			points_of_interest.append(point.global_position)
+
+
 ## Goes through all assigned SpawnAreas and sets their terrain zone.
 func _set_terrain_for_spawn_areas() -> void:
-	var areas_container: Node3D = get_node(SPAWN_AREAS_NAME)
+	# Terrain zone is not needed for the running of tools logic for SpawnArea
+	# nodes.
+	if Engine.is_editor_hint():
+		return
+	var areas_container: Node = get_node(SPAWN_AREAS_NAME)
 	for area: Node in areas_container.get_children():
 		if area is SpawnArea:
 			area.set_terrain_zone(self)
