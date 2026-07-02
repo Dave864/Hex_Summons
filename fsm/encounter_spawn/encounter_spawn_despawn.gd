cd71435423d23de50ffbe0a83bdd9f84634643b6
Dave864 <dmacpherson864@gmail.com>
2026-07-02 03:35:06 -0700

    p0.12-spawn_behavior: Added initial logic for despawning and added it to the fsm.

--- fsm/encounter_spawn/encounter_spawn_despawn.gd
+++ fsm/encounter_spawn/encounter_spawn_despawn.gd
@@ -1,4 +1,3 @@
-@abstract
 class_name EncounterSpawnDespawn
 extends EncounterSpawnState
 ## Base class for the logic of the `Despawn` state.
@@ -7,33 +6,27 @@ extends EncounterSpawnState
 ## invisible before the node itself is removed from the scene. The transition to
 ## invisibility could have EncounterSpawn stay still, move away from the
 ## the direction it was previosly traveling, or continue moving in its last
-## travel direction. The specific behavior for each EncounterSpawn type must be
-## defined in derived classes.
+## travel direction.
 
 
 ## Virtual function. Called by the state machine upon changing the active state.
 ## The `msg` parameter is a dictionary with arbitrary data the state can use to
 ## initialize itself.
 func enter(_msg: Dictionary[Variant, Variant] = {}) -> void:
-	pass
-
-
-## Virtual function. Receives events from the `_unhandled_input()` callback.
-func handle_input(_event: InputEvent) -> void:
-	pass
+	enc_spawn.set_active(false)
+	enc_spawn.sprite.connect(
+			"transition_finished",
+			Callable(self, "_on_EncounterSpawnSprite_transition_finished")
+	)
+	enc_spawn.sprite.ready_despawn_transition()
 
 
 ## Virtual function. Corresponds to the `_process()` callback.
-func update(_delta: float) -> void:
-	pass
+func update(delta: float) -> void:
+	enc_spawn.sprite.progress_transition(delta)
 
 
-## Virtual function. Corresponds to the `_physics_process()` callback.
-func physics_update(_delta: float) -> void:
-	pass
-
-
-## Virtual function. Called by the state machine before changing the active
-## state. Use this function to clean up the state.
-func exit() -> void:
-	pass
+## Queue EncounterSpawn for deletion when its sprite completes the transition.
+func _on_EncounterSpawnSprite_transition_finished() -> void:
+	enc_spawn.emit_despawned()
+	enc_spawn.queue_free()
