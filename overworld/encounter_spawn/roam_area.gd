0be06c17eeaf5925e814b5edf5bc0dfdc6caa3a9
Dave864 <dmacpherson864@gmail.com>
2026-07-08 01:55:37 -0700

    p0.12-spawn_behavior: Updated logic to calculate random point instead of poll random points.

--- overworld/encounter_spawn/roam_area.gd
+++ overworld/encounter_spawn/roam_area.gd
@@ -18,8 +18,8 @@ var min_distance: float = 0.0
 var _y_cast: RayCast3D
 ## A collection of random points.
 var _points_pool: PackedVector3Array
-## The index of the next point in the queue.
-var _next_index: int
+## The index of the current point in the queue.
+var _current_index: int
 
 
 ## Set up the initial points pool when entering the scene tree.
@@ -28,9 +28,10 @@ func _ready() -> void:
 	# rotation of this RoamArea.
 	_y_cast.global_rotation = Vector3.ZERO
 	# Negative index indicates that the pool has not been set yet.
-	_next_index = -1
-	_update_points_pool()
-	_next_index = 0
+	_current_index = -1
+	#_update_points_pool()
+	_points_pool[0] = _determine_point(_current_index)
+	_current_index = 0
 
 
 ## Defines a new roam area.
@@ -59,29 +60,30 @@ func _init(new_radius: float, new_minimum: float) -> void:
 
 ## Gets the next point to roam to.
 func get_next_point() -> Vector3:
-	var next_point := _points_pool[_next_index]
-	_next_index = _next_index + 1 if _next_index < POINT_QUEUE_LIMIT - 1 else 0
-	if _next_index == 0:
-		_update_points_pool()
-	return next_point
+	var prior_index := _current_index
+	_current_index = posmod(_current_index + 1, POINT_QUEUE_LIMIT)
+	_points_pool[_current_index] = _determine_point(prior_index)
+	#if _next_index == 0:
+		#_update_points_pool()
+	return _points_pool[_current_index]
 
 
 ## Updates the points pool.
 func _update_points_pool() -> void:
 	_points_pool[0] = _determine_point(
-			_next_index if _next_index < 0 else POINT_QUEUE_LIMIT - 1
+			_current_index if _current_index < 0 else POINT_QUEUE_LIMIT - 1
 	)
 	for i: int in range(1, POINT_QUEUE_LIMIT):
 		_points_pool[i] = _determine_point(i - 1)
 
 
 ## Gets a random point defined by the roam area.
-func _determine_point(previous_point_index: int) -> Vector3:
+func _determine_point(prior_index: int) -> Vector3:
 	var ray_origin := Vector3.ZERO
-	if previous_point_index < 0 or previous_point_index >= POINT_QUEUE_LIMIT:
+	if prior_index < 0 or prior_index >= POINT_QUEUE_LIMIT:
 		ray_origin = _cast_origin_in_area()
 	else:
-		var prior_point := _points_pool[previous_point_index]
+		var prior_point := _points_pool[prior_index]
 		ray_origin = _cast_origin_from_previous(prior_point)
 	# Apply global Euler rotation to match the ray cast position to RoamArea's
 	# orientation.
@@ -105,21 +107,24 @@ func _cast_origin_in_area() -> Vector3:
 
 ## Gets a random ray cast origin that is at least a minimum distance away from
 ## the last roam point.
-func _cast_origin_from_previous(prior_roam_point: Vector3) -> Vector3:
+func _cast_origin_from_previous(last_roam_point: Vector3) -> Vector3:
 	# Set position reference to be at origin so that the random point
 	# calculations are easier.
-	prior_roam_point -= global_position
-	var prior_pos := Vector2(prior_roam_point.x, prior_roam_point.z)
-	# prior_point.y = 0.0
+	last_roam_point -= global_position
+	var prior_pos := Vector2(last_roam_point.x, last_roam_point.z)
+	var ray_vector := _get_random_direction(prior_pos)
+	var xz_pos := _get_random_point_on_ray(prior_pos, ray_vector)
+	return Vector3(xz_pos.x, 0.0, xz_pos.y)
+	#last_roam_point.y = 0.0
 	#var ray_origin := Vector3.ZERO
 	## Only look at a few points to prevent stalling.
 	#for i: int in 10:
 		#ray_origin = _cast_origin_in_area()
-		#if ray_origin.distance_squared_to(prior_point) > pow(min_distance, 2.0):
+		#if (
+			#ray_origin.distance_squared_to(last_roam_point)
+			#> pow(min_distance, 2.0)
+		#):
 			#return ray_origin
-	var ray_vector := _get_random_direction(prior_pos)
-	var xz_pos := _get_random_point_on_ray(prior_pos, ray_vector)
-	return Vector3(xz_pos.x, 0.0, xz_pos.y)
 	#return ray_origin
 
 
@@ -128,14 +133,15 @@ func _cast_origin_from_previous(prior_roam_point: Vector3) -> Vector3:
 ## within the boundaries of RoamArea.
 ## Reference: https://www.johndcook.com/blog/2023/08/27/intersect-circles/
 func _get_random_direction(exclusion_center: Vector2) -> Vector2:
-	# Roam area centered at origin.
+	# Roam area centered at origin, so distance to exclusion center from
+	# RoamArea center is length of vector.
 	var d := exclusion_center.length()
 	# Rename relevant parameters for conciseness.
 	var r0 := radius
 	var r1 := min_distance
-	# Exclusion zone does not overlap boundaries of RoamArea or is centered at 
-	# RoamArea, so any direction is valid.
-	if is_zero_approx(d) or d < abs(r0 - r1) or d > r0 + r1:
+	if d < abs(r0 - r1) or d > r0 + r1:
+		# Exclusion zone does not overlap boundaries of RoamArea so any
+		# direction is valid.
 		return Vector2.from_angle(randf() * TAU).normalized()
 	var u := exclusion_center.normalized()
 	var x_vec := (pow(d, 2.0) - pow(r1, 2.0) + pow(r0, 2.0)) * u / (2 * d)
@@ -148,12 +154,16 @@ func _get_random_direction(exclusion_center: Vector2) -> Vector2:
 				* (d + r0 + r1)
 		) / d * 0.5
 	)
-	var intersect_1 := x_vec + u_perp * a_half
-	var intersect_2 := x_vec - u_perp * a_half
+	var intersect_1 := x_vec - u_perp * a_half
+	var intersect_2 := x_vec + u_perp * a_half
 	var v1 := intersect_1 - exclusion_center
 	var v2 := intersect_2 - exclusion_center
 	var angle := v1.angle_to(v2)
-	return v1.rotated(randf() * angle).normalized()
+	# Want to get the value of the angle that points towards the center of
+	# RoamArea. Negative angle points away from RoamArea center.
+	if angle < 0.0:
+		angle += TAU
+	return v1.normalized().rotated(randf() * angle)
 
 
 ## Gets a random position along a ray cast in a given direction. The returned
@@ -172,7 +182,7 @@ func _get_random_point_on_ray(
 	var max_dist: float
 	if discriminent < 0.0:
 		printerr("Cannot find random point in RoamArea.")
-		return Vector2.ZERO
+		return ray_start
 	elif is_zero_approx(discriminent):
 		max_dist = -b / (2.0 * a)
 	else:
