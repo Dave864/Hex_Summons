ad7d6bd3d1da44eb90a70218f9f22c0f8d77c836
Dave864 <dmacpherson864@gmail.com>
2026-07-17 23:38:06 -0700

    p0.12-rotating_sprite: Added logic to update animation based on orientation.
    Animations need to change to reflect the display orientation.

--- utils/rotating_sprite_3d.gd
+++ utils/rotating_sprite_3d.gd
@@ -23,7 +23,10 @@ var facing_direction := Vector2.RIGHT:
 		_determine_orientation()
 
 ## The orientation that is being displayed.
-var _display_orientation: DisplayOrientation
+var _orientation: DisplayOrientation
+## The animation group that is currently playing. The prefix for the animation
+## name.
+var _animation_group: String
 
 ## The current camera for the scene.
 @onready var _camera: Camera3D = get_viewport().get_camera_3d()
@@ -32,12 +35,34 @@ var _display_orientation: DisplayOrientation
 ## Sets the sprite as billboard and gets the starting orientation.
 func _ready() -> void:
 	billboard = BaseMaterial3D.BILLBOARD_ENABLED
+	texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST
+	_animation_group = "default"
 	_determine_orientation()
 
 
 ## Updates the orientation to account for camera adjustements.
 func _process(_delta: float) -> void:
 	_determine_orientation()
+	_match_animation_to_orientation()
+
+
+## Sets the animation to "idle".
+func play_idle() -> void:
+	_play_animation("idle")
+
+
+## Sets the animation to "movement".
+func play_movement() -> void:
+	_play_animation("move")
+
+
+## Starts the specified animation group, choosing the appropriate animation
+## based on orientation.
+func _play_animation(next_group: String) -> void:
+	_animation_group = next_group
+	frame = 0
+	frame_progress = 0.0
+	_match_animation_to_orientation()
 
 
 ## Determines the orientation the display should be in.
@@ -50,21 +75,34 @@ func _determine_orientation() -> void:
 	var dot := camera_dir.dot(facing_direction)
 	var right_facing := camera_dir.angle_to(facing_direction) >= 0.0
 	if dot >= 0.75:
-		_display_orientation = DisplayOrientation.BACK
+		_orientation = DisplayOrientation.BACK
 	elif dot < 0.75 and dot > 0.25:
-		_display_orientation = (
+		_orientation = (
 				DisplayOrientation.BACK_RIGHT if right_facing
 				else DisplayOrientation.BACK_LEFT
 		)
 	elif dot <= 0.25 and dot >= -0.25:
-		_display_orientation = (
+		_orientation = (
 				DisplayOrientation.RIGHT if right_facing
 				else DisplayOrientation.LEFT
 		)
 	elif dot < -0.25 and dot > 0.75:
-		_display_orientation = (
+		_orientation = (
 				DisplayOrientation.FRONT_RIGHT if right_facing
 				else DisplayOrientation.FRONT_LEFT
 		)
 	else:
-		_display_orientation = DisplayOrientation.FRONT
+		_orientation = DisplayOrientation.FRONT
+
+
+## Changes the active animation to match the current orientation.
+func _match_animation_to_orientation() -> void:
+	var current_frame := frame
+	var current_progress := frame_progress
+	animation = _animation_group + "_" + _orientation_string()
+	set_frame_and_progress(current_frame, current_progress)
+
+
+## Gets a string describing the orientation.
+func _orientation_string() -> String:
+	return String(DisplayOrientation.keys()[_orientation]).to_lower()
