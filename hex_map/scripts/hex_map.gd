32e9efeadb4495680cb238a43cfe343ce834765f
Dave864 <dmacpherson864@gmail.com>
2026-07-18 03:18:35 -0700

    p0.12-rotating_sprite: Added logic to set initial facing direction.
    By default, the facing direction is to the right. Set initial direciton for encounters
    to be pointing towards origin along one of the hex directions. This is so that the
    characters align with the directions of travel.

--- hex_map/scripts/hex_map.gd
+++ hex_map/scripts/hex_map.gd
@@ -62,6 +62,12 @@ func get_z_count() -> int:
 func place_character_at_tile(character: Character, tile_index: int) -> void:
 	var pos: Vector3 = get_tile_at(tile_index).get_character_position()
 	character.position = pos
+	# Set initial facing direction to point to origin.
+	var hex_dir := HexUtil.get_hex_direction(
+			Vector2(-pos.x, -pos.z).normalized()
+	)
+	var facing_direction := Vector2.UP.rotated(HexUtil.dir_rotation(hex_dir))
+	character.character_sprite.facing_direction = facing_direction.normalized()
 
 
 ## Retrieve the map tiles of this hex map.
