7a11c95216370c79eeb65c054a3d01a5aac4cd01
Dave864 <dmacpherson864@gmail.com>
2026-07-19 00:40:41 -0700

    p0.12-rotating_sprite: Changed sprite of OverworldAvatar to be a rotating sprite.
    Sprite should rotate as the player moves around the map.

--- overworld/overworld_avatar/overworld_avatar.gd
+++ overworld/overworld_avatar/overworld_avatar.gd
@@ -6,6 +6,15 @@ extends CharacterBody3D
 ## The speed the avatar moves at.
 const SPEED = 7.0
 
+## The sprite for the character.
+@onready var _sprite: RotatingSprite3D = $RotatingSprite3D
+
+
+## Orient the sprite to face forwards.
+func _ready() -> void:
+	_sprite.facing_direction = Vector2.DOWN
+	_sprite.play_idle()
+
 
 ## Processes the physics of moving around.
 func _physics_process(delta: float) -> void:
@@ -20,7 +29,14 @@ func _physics_process(delta: float) -> void:
 			"player_move_u",
 			"player_move_d"
 	)
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	# Only update facing direction if significant input has been provided.
+	if not input_dir.is_zero_approx():
+		_sprite.play_movement()
+		_sprite.facing_direction = input_dir.normalized()
+	else:
+		_sprite.play_idle()
+	var input_dir_vector := Vector3(input_dir.x, 0.0, input_dir.y)
+	var direction := (transform.basis * input_dir_vector).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
