bc52727a11768493001ca869c7fc8cfc89842ff7
Dave864 <dmacpherson864@gmail.com>
2026-08-06 17:43:56 -0700

    p0.13-travel_points: Created empty TravelPoints script.
    This class will be a tool for managing travel points for spawners.

---
+++ overworld/encounter_zone/travel_points.gd
@@ -0,0 +1,18 @@
+class_name TravelPoints
+extends Node3D
+## Holds a collection of points in 3D space that serve as travel points for
+## overworld characters.
+##
+## Defines a helper that populates a defined area with a selected number of
+## points. Places new points on the nav mesh the area is above or on the plane
+## if no such mesh is found.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
