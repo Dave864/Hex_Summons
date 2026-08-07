4d04800465c0898b3d9f8ded03395c0bdf51e27d
Dave864 <dmacpherson864@gmail.com>
2026-08-06 17:07:22 -0700

    p0.12-spawn_behavior: Updated default values for SpawnParameters.

--- overworld/encounter_spawn/spawn_parameters.gd
+++ overworld/encounter_spawn/spawn_parameters.gd
@@ -11,13 +11,13 @@ extends Resource
 @export_range(1.0, 15.0, 0.01) var speed_reaction := 8.0
 @export_group("State Travel Distance Until Despawn", "despawn_distance_")
 ## The distance the spawner can travel while in idle before despawining.
-@export_range(0.0, 10.0, 0.01) var despawn_distance_idle := 1.5
+@export_range(0.0, 5.0, 0.01) var despawn_distance_idle := 1.5
 ## The distance the spawner can travel while in reaction before despawning.
-@export_range(0.0, 10.0, 0.01) var despawn_distance_reaction := 2.0
+@export_range(0.0, 3.0, 0.01) var despawn_distance_reaction := 1.0
 @export_group("Alert State Details")
 ## The time spent in seconds on alert before the character jumps to its reaction.
 @export_range(0.0, 10.0, 0.01) var alert_time := 3.0
 ## How close something must get before it is detected.
 @export_range(1.0, 20.0, 0.01) var alert_radius := 8.0
 ## How close a detected body must be before a reaction is triggered.
-@export_range(1.0, 20.0, 0.01) var reaction_radius := 4.0
+@export_range(1.0, 20.0, 0.01) var reaction_radius := 5.0
